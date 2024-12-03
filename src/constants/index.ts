a205b477128405c4627fde6f229fc24eaa11ce95
Sergey Kabanets <skabanets97@gmail.com>
2024-12-04 01:18:23 +0200

    Add serviceLinks constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export * from "./routes";
 export * from "./industries";
 export * from "./themeMode";
 export * from "./navLinks";
+export * from "./serviceLinks";
---
+++ src/constants/serviceLinks.ts
@@ -0,0 +1,5 @@
+export const serviceLinks = [
+  { id: 1, text: "End-to-End Application Development" },
+  { id: 2, text: "Artificial Intelligence (AI) Integration" },
+  { id: 3, text: "Agile Methodologies" },
+];
