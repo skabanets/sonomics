a939733858d731e1460febaa375885aa6fac3580
Sergey Kabanets <skabanets97@gmail.com>
2024-12-03 23:59:26 +0200

    Add Why-us section

---
+++ src/sections/Home/WhyUs.tsx
@@ -0,0 +1,16 @@
+export const WhyUs = () => {
+  return (
+    <section className="py-[100px]">
+      <div className="align-center mx-auto flex w-[920px] flex-col items-center justify-center gap-2.5">
+        <h6 className="text-secondaryTextColor">Why Sonomics</h6>
+        <h2 className="text-center leading-[54px]">
+          We're not just about IT solutions — we're about driving{" "}
+          <span className="text-accentYellowColor">
+            your business <br /> growth
+          </span>
+          , ensuring stability and <br /> fostering expertise
+        </h2>
+      </div>
+    </section>
+  );
+};
--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,1 +1,2 @@
+export * from "./Home/WhyUs";
 export * from "./Home/Industries";
