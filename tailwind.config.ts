9f03daaf1d6d6c6f142ecc45d6308b2eb4bfca30
Sergey Kabanets <skabanets97@gmail.com>
2024-12-03 15:00:53 +0200

    Add font settings and color variables to Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+    },
     screens: {
       "sm-max": { max: "374.98px" },
       md: "768px",
       lg: "1440px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        primaryBgColor: "var(--primary-bg)",
+        secondaryBgColor: "var(--secondary-bg)",
+        blackBgColor: "var(--black-bg)",
+        darkBgColor: "var(--dark-bg)",
+
+        mainTextColor: "var(--main-text)",
+        secondaryTextColor: "var(--secondary-text)",
+        whiteTextColor: "var(--white-text)",
+        secondaryLightTextColor: "var(--secondary-light-text)",
+
+        accentRedColor: "var(--accent-red)",
+        accentYellowColor: "var(--accent-yellow)",
+        accentOrangeColor: "var(--accent-orange)",
+      },
+    },
   },
   corePlugins: {
     container: false,
