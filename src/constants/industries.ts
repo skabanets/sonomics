139f691ff05814f5ca8d0f50aac76889ca3dddc7
Sergey Kabanets <skabanets97@gmail.com>
2024-12-03 22:57:01 +0200

    Add industries constant

---
+++ src/constants/industries.ts
@@ -0,0 +1,23 @@
+export const industries = [
+  {
+    id: 1,
+    name: "Fintech",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+    icon: "fintech",
+  },
+  {
+    id: 2,
+    name: "Healthcare",
+    description:
+      "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+    icon: "healthcare",
+  },
+  {
+    id: 3,
+    name: "Ecommerce",
+    description:
+      "Ut enim ad minim veniam, quis nostrud. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+    icon: "ecommerce",
+  },
+];
