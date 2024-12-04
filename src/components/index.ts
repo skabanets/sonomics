a9192e642994c08f159bc57253c71e418068a7a2
VasylPopaz <pafos1997@gmail.com>
2024-12-04 14:05:19 +0100

    Add ThemeToggle component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export * from "./Loader";
 export * from "./Icon";
 export * from "./IndustryCard";
 export * from "./ViewAllLink";
+export * from "./Logo";
+export * from "./ThemeToggle";
+export * from "./Button";
