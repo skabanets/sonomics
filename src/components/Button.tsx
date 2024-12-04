80caeccc2c74cccf045f549bd6b57e9739fe1b96
VasylPopaz <pafos1997@gmail.com>
2024-12-04 17:36:32 +0100

    Add arrow icon to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,19 @@
+import { Icon } from "../components";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   width?: string;
 }
 
-export const Button = ({ label, width = "169", ...rest }: ButtonProps) => {
+export const Button = ({ label, width = "w-[170px]", ...rest }: ButtonProps) => {
   return (
     <button
       type="button"
-      style={{ width: `${width}px` }}
-      className="h-[47px] rounded-[90px] bg-accentRedColor px-[25px] py-[10px] text-[18px] font-medium leading-[1.5] text-whiteTextColor"
+      className={`flex h-[47px] ${width} items-center justify-center gap-[10px] rounded-[90px] bg-accentRedColor px-[23px] py-[10px] text-[18px] font-medium leading-[1.5] text-whiteTextColor`}
       {...rest}
     >
       {label}
+      <Icon id="arrow-button" className="size-[15px] fill-whiteTextColor" />
     </button>
   );
 };
