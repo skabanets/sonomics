d18e8b65b5cafa4eac2f98a2fdb380bfd3293eb6
Sergey Kabanets <skabanets97@gmail.com>
2024-12-04 23:55:23 +0200

    Add caseStudies constant

---
+++ src/constants/caseStudies.ts
@@ -0,0 +1,38 @@
+export const caseStudies = [
+  {
+    id: 1,
+    link: "/case-study",
+    name: "Case study name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+    image: "case1_1x",
+    retinaImage: "case1_2x",
+  },
+  {
+    id: 2,
+    link: "/case-study",
+    name: "Case study name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+    image: "case2_1x",
+    retinaImage: "case2_2x",
+  },
+  {
+    id: 3,
+    link: "/case-study",
+    name: "Case study name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+    image: "case1_1x",
+    retinaImage: "case1_2x",
+  },
+  {
+    id: 4,
+    link: "/case-study",
+    name: "Case study name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+    image: "case2_1x",
+    retinaImage: "case2_2x",
+  },
+];
