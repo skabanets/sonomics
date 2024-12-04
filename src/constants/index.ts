cafdd387599bbdc993039ac953608de3d6047a86
VasylPopaz <pafos1997@gmail.com>
2024-12-04 14:52:51 +0100

    Add constant for navigation links

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,7 @@
 export * from "./routes";
 export * from "./industries";
+export * from "./themeMode";
+export * from "./navLinks";
 export * from "./serviceLinks";
+export * from "./themeMode";
+export * from "./navLinks";
---
+++ src/constants/navLinks.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { text: "Services", link: "/services" },
+  { text: "Industries", link: "/industries" },
+  { text: "Case Study", link: "/case-study" },
+  { text: "About", link: "/about" },
+];
