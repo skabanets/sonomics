f13f8a7d190f6a859e4cf6bbc602ab5c9183c9e9
VasylPopaz <pafos1997@gmail.com>
2024-12-04 14:12:02 +0100

    Update Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,15 @@
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   width?: string;
 }
 
-export const Button = ({ label, width = "169" }: ButtonProps) => {
+export const Button = ({ label, width = "169", ...rest }: ButtonProps) => {
   return (
     <button
       type="button"
       style={{ width: `${width}px` }}
       className="h-[47px] rounded-[90px] bg-accentRedColor px-[25px] py-[10px] text-[18px] font-medium leading-[1.5] text-whiteTextColor"
+      {...rest}
     >
       {label}
     </button>
