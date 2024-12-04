492ccde1d5f28ed63c21b19765ab1a6155ed0947
Sergey Kabanets <skabanets97@gmail.com>
2024-12-04 22:32:59 +0200

    Refactor folder structure for images and add re-export file for images

---
+++ src/assets/index.ts
@@ -0,0 +1,34 @@
+// Cases
+import case1_1x from "./images/cases/case-1@1x.webp";
+import case1_2x from "./images/cases/case-1@2x.webp";
+import case2_1x from "./images/cases/case-2@1x.webp";
+import case2_2x from "./images/cases/case-2@2x.webp";
+
+// Team
+import ceo_1x from "./images/team/ceo@1x.webp";
+import ceo_2x from "./images/team/ceo@2x.webp";
+
+// Common
+import our_mission_1x from "./images/common/our-mission@1x.webp";
+import our_mission_2x from "./images/common/our-mission@2x.webp";
+import barcelona_1x from "./images/common/barcelona@1x.webp";
+import barcelona_2x from "./images/common/barcelona@2x.webp";
+
+export const images = {
+  casesImages: {
+    case1_1x,
+    case1_2x,
+    case2_1x,
+    case2_2x,
+  },
+  teamImages: {
+    ceo_1x,
+    ceo_2x,
+  },
+  commonImages: {
+    our_mission_1x,
+    our_mission_2x,
+    barcelona_1x,
+    barcelona_2x,
+  },
+};
