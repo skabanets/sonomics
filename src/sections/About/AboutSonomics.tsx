f2fb72671bec88809e026b4cf73061dfb2652a5d
VasylPopaz <pafos1997@gmail.com>
2024-12-04 22:04:42 +0100

    Add AboutSonomics section

---
+++ src/sections/About/AboutSonomics.tsx
@@ -0,0 +1,14 @@
+export const AboutSonomics = () => {
+  return (
+    <section className="pb-[80px] pt-[100px]">
+      <div className="container">
+        <h6 className="mb-[20px] text-secondaryTextColor">About Sonomics</h6>
+        <h1>
+          We're <span className="text-accentYellowColor"> driving </span> your
+          <span className="text-accentYellowColor"> business</span> growth, ensuring stability and
+          fostering expertise.
+        </h1>
+      </div>
+    </section>
+  );
+};
