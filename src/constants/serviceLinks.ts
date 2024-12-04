b36d585098fcc8df020ae987e015e992a141c407
Sergey Kabanets <skabanets97@gmail.com>
2024-12-04 19:02:23 +0200

    Update industries and serviceLinks constants

--- src/constants/serviceLinks.ts
+++ src/constants/serviceLinks.ts
@@ -1,5 +1,5 @@
 export const serviceLinks = [
-  { id: 1, text: "End-to-End Application Development" },
-  { id: 2, text: "Artificial Intelligence (AI) Integration" },
-  { id: 3, text: "Agile Methodologies" },
+  { id: 1, text: "End-to-End Application Development", link: "/services" },
+  { id: 2, text: "Artificial Intelligence (AI) Integration", link: "/services" },
+  { id: 3, text: "Agile Methodologies", link: "/services" },
 ];
