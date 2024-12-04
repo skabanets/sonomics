ce96ecc7b892199e3c3be95cf10978f937fa38fc
VasylPopaz <pafos1997@gmail.com>
2024-12-05 00:43:50 +0100

    Add SonomicsNumbers section

---
+++ src/sections/About/SonomicsNumbers.tsx
@@ -0,0 +1,28 @@
+import { Divider } from "../../components";
+
+import { sonomicsNumbers } from "../../constants";
+
+export const SonomicsNumbers = () => {
+  return (
+    <section className="py-[100px]">
+      <div className="container flex justify-between">
+        <h2 className="w-[300px] pt-[20px]">Sonomics in numbers</h2>
+        <ul className="w-[774px]">
+          {sonomicsNumbers.map(({ title, number }, index) => (
+            <li
+              key={index}
+              className="flex flex-wrap items-center justify-between gap-y-[28px] pt-[30px] first:pt-0"
+            >
+              <p className="text-[120px] font-medium leading-[156px] text-accentRedColor">
+                {number}+
+              </p>
+              <h4 className="w-[490px]">{title}</h4>
+
+              <Divider />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
--- src/sections/index.ts
+++ src/sections/index.ts
@@ -2,3 +2,5 @@ export * from "./Home/WhyUs";
 export * from "./Home/Industries";
 export * from "./Home/Services";
 export * from "./About/AboutSonomics";
+export * from "./About/SonomicsNumbers";
+export * from "./About/SuccessfulStories";
