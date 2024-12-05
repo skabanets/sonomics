202babc75601c6043c1ecd0c8a63b5bbe5122bd4
Sergey Kabanets <skabanets97@gmail.com>
2024-12-05 15:57:22 +0200

    Update CaseStudies section

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,6 +1,7 @@
 export * from "./Home/WhyUs";
 export * from "./Home/Industries";
 export * from "./Home/Services";
+export * from "./Home/CaseStudies";
 export * from "./About/AboutSonomics";
 export * from "./About/SonomicsNumbers";
 export * from "./About/SuccessfulStories";
