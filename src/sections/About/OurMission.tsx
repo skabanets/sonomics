6f11b74a9214fe835c07d6d9fdd880966519d125
VasylPopaz <pafos1997@gmail.com>
2024-12-05 21:46:50 +0100

    Update OurMission styles

--- src/sections/About/OurMission.tsx
+++ src/sections/About/OurMission.tsx
@@ -2,9 +2,10 @@ import { images } from "../../assets";
 
 export const OurMission = () => {
   const { commonImages } = images;
+
   return (
-    <section>
-      <div className="mx-auto flex max-w-[1440px]">
+    <section className="bg-ourMisionBgColor">
+      <div className="mx-auto flex max-h-[600px]">
         <img
           srcSet={`${commonImages.our_mission_1x} 1x, ${commonImages.our_mission_2x} 2x`}
           src={commonImages.our_mission_1x}
@@ -13,7 +14,7 @@ export const OurMission = () => {
           height="600"
           className="w-1/2 object-cover"
         />
-        <div className="bg-ourMisionBgColor flex w-1/2 flex-col justify-center pl-[70px] pr-[150px] transition">
+        <div className="flex w-1/2 flex-col justify-center pl-[70px] pr-[150px] transition">
           <h2 className="mb-[24px] text-whiteTextColor">Our mission</h2>
           <h4 className="mb-[20px] text-accentYellowColor">End-to-End Application Development</h4>
           <p className="big-text text-whiteTextColor">
