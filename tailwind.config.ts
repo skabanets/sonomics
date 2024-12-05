985a1c7ac7b1f8812d52171730d1eec5dcef8b71
VasylPopaz <pafos1997@gmail.com>
2024-12-05 14:36:15 +0100

    Add TeamMemberCard component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "before:bg-accentRedColor",
+    "before:bg-accentYellowColor",
+    "before:bg-accentOrangeColor",
+  ],
   theme: {
     fontFamily: {
       montserrat: ["Montserrat", "sans-serif"],
