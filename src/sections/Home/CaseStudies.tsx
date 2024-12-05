433d73d47102c945ee046f74361c843217dff30a
Sergey Kabanets <skabanets97@gmail.com>
2024-12-05 15:47:14 +0200

    Add CaseStudies section

---
+++ src/sections/Home/CaseStudies.tsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+
+import { CaseCard, CustomTitle } from "../../components";
+
+import { caseStudies, routes } from "../../constants";
+import { CaseStudy } from "../../types";
+
+export const CaseStudies = () => {
+  const styles = [
+    {
+      containerStyles: "w-[540px] mr-auto",
+      width: 540,
+      height: 500,
+    },
+    {
+      containerStyles: "w-[620px]",
+      width: 620,
+      height: 370,
+    },
+    {
+      containerStyles: "w-[620px]",
+      width: 620,
+      height: 370,
+    },
+    {
+      containerStyles: "w-[540px] ml-auto",
+      width: 540,
+      height: 500,
+    },
+  ];
+
+  const styledCaseStudies = caseStudies.map((caseStudy, index) => ({
+    ...caseStudy,
+    styles: styles[index],
+  }));
+
+  const columns = [styledCaseStudies.slice(0, 2), styledCaseStudies.slice(2, 4)];
+
+  return (
+    <section className="pb-[100px] pt-[80px]">
+      <div className="container flex flex-col gap-[60px]">
+        <div className="flex items-center justify-between">
+          <h2>Case studies</h2>
+          <Link to={routes.CaseStudy} className="view-link">
+            <CustomTitle
+              type="h4"
+              wrapperStyles="flex items-center gap-[10px]"
+              iconColor="fill-accentRedColor"
+              titleStyles="text-accentRedColor"
+            />
+          </Link>
+        </div>
+        <div className="flex gap-[40px]">
+          {columns.map((column, columnIndex) => (
+            <div key={columnIndex} className="flex flex-col gap-[80px]">
+              {column.map((caseStudy) => (
+                <div className="w-[620px]" key={caseStudy.id}>
+                  <CaseCard
+                    caseStudy={caseStudy as CaseStudy}
+                    containerStyles={caseStudy.styles.containerStyles}
+                    imageWidth={caseStudy.styles.width}
+                    imageHeight={caseStudy.styles.height}
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
