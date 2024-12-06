50cedf6b9b7c9cb16602446dc376b0f4d125bed0
VasylPopaz <pafos1997@gmail.com>
2024-12-06 16:49:02 +0100

    Add hover effects for navigation links

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -3,16 +3,17 @@ import { NavLink } from "react-router-dom";
 import { navLinks } from "../constants";
 
 interface NavMenuProps {
-  className: string;
+  listClass: string;
+  linkClass: string;
 }
 
-export const NavMenu = ({ className }: NavMenuProps) => {
+export const NavMenu = ({ listClass, linkClass }: NavMenuProps) => {
   return (
-    <ul className={className}>
+    <ul className={listClass}>
       {navLinks.map(({ text, link }, index) => (
         <li key={index}>
           {" "}
-          <NavLink className="py-[5px]" to={link}>
+          <NavLink className={`py-[5px] ${linkClass}`} to={link}>
             {text}
           </NavLink>{" "}
         </li>
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,13 +2,16 @@ import { Button, Logo, NavMenu, ThemeToggle } from "../components";
 
 export const Header = () => {
   return (
-    <header className="pt-[40px]">
+    <header className="header pt-[40px]">
       <div className="container flex items-center">
         <nav className="flex w-full justify-between">
           <Logo applyThemeColor />
           <div className="flex items-center gap-[50px]">
             <ThemeToggle />
-            <NavMenu className="flex gap-[50px] font-medium text-mainTextColor transition" />
+            <NavMenu
+              listClass="flex gap-[50px] font-medium text-mainTextColor transition"
+              linkClass="hover:text-navlinkActiveColor focus-visible:text-navlinkActiveColor active:text-navlinkActiveColor transition"
+            />
             <Button label="Contact us" />
           </div>
         </nav>
