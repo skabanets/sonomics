feed150c870438180bcd976b336b18c049559cd8
VasylPopaz <pafos1997@gmail.com>
2024-12-06 16:49:02 +0100

    Add hover effects for navigation links

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -6,7 +6,7 @@ import { socialLinks } from "../constants";
 
 export const Footer = () => {
   return (
-    <footer className="bg-blackBgColor pb-[20px] pt-[80px]">
+    <footer className="footer bg-blackBgColor pb-[20px] pt-[80px]">
       <div className="container flex pb-[40px]">
         <div className="mr-auto w-[281px] space-y-[22px]">
           {" "}
@@ -14,17 +14,30 @@ export const Footer = () => {
           <h2 className="text-whiteTextColor">Let&#8217;s work together</h2>
         </div>
         <nav className="mr-[130px]">
-          <NavMenu className="flex flex-col gap-[30px] text-whiteTextColor" />
+          <NavMenu
+            listClass="flex flex-col gap-[30px] text-whiteTextColor"
+            linkClass="hover:text-accentYellowColor focus-visible:text-accentYellowColor active:text-accentYellowColor"
+          />
         </nav>
         <div>
           <address className="mb-[50px] not-italic">
             <p className="label mb-[15px] text-secondaryLightTextColor">Contact us</p>
             <ul className="bold-text flex flex-col gap-[15px] text-whiteTextColor">
               <li>
-                <Link to="tel:+12345678990">+1 234 567-89-90</Link>
+                <Link
+                  to="tel:+12345678990"
+                  className="transition hover:text-accentYellowColor focus-visible:text-accentYellowColor active:text-accentYellowColor"
+                >
+                  +1 234 567-89-90
+                </Link>
               </li>
               <li>
-                <Link to="mailto:info@sonomics.com">info@sonomics.com</Link>
+                <Link
+                  to="mailto:info@sonomics.com"
+                  className="transition hover:text-accentYellowColor focus-visible:text-accentYellowColor active:text-accentYellowColor"
+                >
+                  info@sonomics.com
+                </Link>
               </li>
             </ul>
           </address>
@@ -35,7 +48,7 @@ export const Footer = () => {
                   to={link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex h-[35px] w-[112px] items-center justify-center rounded-[90px] border border-accentRedColor px-[25px] py-[10px] text-[12px] text-whiteTextColor"
+                  className="flex h-[35px] w-[112px] items-center justify-center rounded-[90px] border border-accentRedColor px-[25px] py-[10px] text-[12px] text-whiteTextColor transition hover:bg-accentRedColor focus-visible:bg-accentRedColor active:bg-accentRedColor"
                 >
                   {text}
                 </Link>
