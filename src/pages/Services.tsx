704bc85bb094da29333f2a14657f1989759f5c47
VasylPopaz <pafos1997@gmail.com>
2024-12-11 22:21:39 +0100

    Update animation

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import { Divider } from "../components";
 
 import { sonomicsNumbers } from "../constants";
-import { AboutSonomics, OurMission } from "../sections";
+import { AboutSonomics, LetsTalk, OurMission, SuccessfulStories, Team } from "../sections";
 
 interface AnimatedNumberProps {
   finalNumber: number;
@@ -45,28 +45,34 @@ const Services = () => {
     <>
       <AboutSonomics />
       <OurMission />
-      <div className="py-[100px]">
-        <ul className="mx-auto w-[774px] overflow-hidden">
-          {sonomicsNumbers.map(({ title, number }, index) => (
-            <motion.li
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              transition={{ duration: 1.5 }}
-              viewport={{ once: true, amount: 0.7 }}
-              key={index}
-              className="flex flex-wrap items-center justify-between gap-y-[4vh] pt-[4vh] first:pt-0"
-            >
-              <AnimatedNumber
-                finalNumber={number}
-                className="text-[120px] font-medium leading-[156px] text-accentRedColor"
-              />
-              <h4 className="w-[490px]">{title}</h4>
+      <section className="py-[100px]">
+        <div className="container relative">
+          <h2 className="sticky top-[100px] z-10 w-[300px] pt-[20px]">Sonomics in numbers</h2>
+          <ul className="ml-auto w-[774px] overflow-hidden">
+            {sonomicsNumbers.map(({ title, number }, index) => (
+              <motion.li
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 1.5 }}
+                viewport={{ once: true, amount: 0.7 }}
+                key={index}
+                className="flex flex-wrap items-center justify-between gap-y-[4vh] pt-[4vh] first:pt-0"
+              >
+                <AnimatedNumber
+                  finalNumber={number}
+                  className="text-[120px] font-medium leading-[156px] text-accentRedColor"
+                />
+                <h4 className="w-[490px]">{title}</h4>
 
-              {index < sonomicsNumbers.length - 1 && <Divider />}
-            </motion.li>
-          ))}
-        </ul>
-      </div>
+                {index < sonomicsNumbers.length - 1 && <Divider />}
+              </motion.li>
+            ))}
+          </ul>
+        </div>
+      </section>
+      <Team />
+      <SuccessfulStories />
+      <LetsTalk />
     </>
   );
 };
