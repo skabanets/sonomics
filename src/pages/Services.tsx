f8dca62add04b2b258dfa08f96c98eb0670af288
VasylPopaz <pafos1997@gmail.com>
2024-12-11 22:11:36 +0100

    Update animation sonomics numbers

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,38 +1,10 @@
-import { animate, motion, useMotionValue, useTransform } from "framer-motion";
-import { useEffect, useState } from "react";
+import { animate, motion, useInView, useMotionValue, useTransform } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 import { Divider } from "../components";
 
 import { sonomicsNumbers } from "../constants";
-
-const Services = () => {
-  return (
-    <div>
-      <ul className="mx-auto w-[774px] overflow-hidden">
-        {sonomicsNumbers.map(({ title, number }, index) => (
-          <motion.li
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            viewport={{ once: true, amount: 0.35 }}
-            key={index}
-            className="flex flex-wrap items-center justify-between gap-y-[4vh] pt-[4vh] first:pt-0"
-          >
-            <AnimatedNumber
-              finalNumber={number}
-              className="text-[120px] font-medium leading-[156px] text-accentRedColor"
-            />
-            <h4 className="w-[490px]">{title}</h4>
-
-            {index < sonomicsNumbers.length - 1 && <Divider />}
-          </motion.li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default Services;
+import { AboutSonomics, OurMission } from "../sections";
 
 interface AnimatedNumberProps {
   finalNumber: number;
@@ -43,19 +15,60 @@ const AnimatedNumber = ({ finalNumber, className }: AnimatedNumberProps) => {
   const count = useMotionValue(0);
   const rounded = useTransform(count, Math.round);
   const [displayValue, setDisplayValue] = useState(0);
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.7 });
 
   useEffect(() => {
-    const unsubscribe = rounded.onChange((latest) => {
-      setDisplayValue(latest);
-    });
+    if (isInView) {
+      const unsubscribe = rounded.onChange((latest) => {
+        setDisplayValue(latest);
+      });
 
-    const animation = animate(count, finalNumber, { duration: 1.5 });
+      const animation = animate(count, finalNumber, { duration: 1.5 });
 
-    return () => {
-      unsubscribe();
-      animation.stop();
-    };
-  }, [count, finalNumber, rounded]);
+      return () => {
+        unsubscribe();
+        animation.stop();
+      };
+    }
+  }, [isInView, count, finalNumber, rounded]);
 
-  return <p className={`${className} tabular-nums`}>{displayValue}+</p>;
+  return (
+    <p ref={ref} className={`${className} tabular-nums`}>
+      {displayValue}+
+    </p>
+  );
 };
+
+const Services = () => {
+  return (
+    <>
+      <AboutSonomics />
+      <OurMission />
+      <div className="py-[100px]">
+        <ul className="mx-auto w-[774px] overflow-hidden">
+          {sonomicsNumbers.map(({ title, number }, index) => (
+            <motion.li
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              viewport={{ once: true, amount: 0.7 }}
+              key={index}
+              className="flex flex-wrap items-center justify-between gap-y-[4vh] pt-[4vh] first:pt-0"
+            >
+              <AnimatedNumber
+                finalNumber={number}
+                className="text-[120px] font-medium leading-[156px] text-accentRedColor"
+              />
+              <h4 className="w-[490px]">{title}</h4>
+
+              {index < sonomicsNumbers.length - 1 && <Divider />}
+            </motion.li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Services;
