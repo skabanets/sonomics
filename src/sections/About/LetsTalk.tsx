41cf973e8fdf3969fd099e7e569125390bfdc650
Sergey Kabanets <skabanets97@gmail.com>
2024-12-13 23:09:37 +0200

    Update LetsTalk section

--- src/sections/About/LetsTalk.tsx
+++ src/sections/About/LetsTalk.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 import { ContactForm, Icon } from "../../components";
 
+import { contacts } from "../../constants";
 import { images } from "../../assets";
 
 export const LetsTalk = () => {
@@ -10,49 +11,29 @@ export const LetsTalk = () => {
   return (
     <section className="bg-themeToggleBgColor py-[80px]">
       <div className="container flex flex-row justify-between">
-        <div className="w-[536px]">
-          <div>
-            <h2>Let’s talk</h2>
-            <p className="big-text mt-[10px]">Any question or remarks? Just write us a message!</p>
-          </div>
-          <ContactForm />
-        </div>
-        <div className="w-[619px]">
-          <div className="mb-[30px] mt-[26px] flex flex-col gap-[15px]">
+        <ContactForm />
+        <div className="w-[620px]">
+          <div className="mb-[30px] mt-[28px] flex flex-col gap-[20px]">
             <p className="big-text">Our contacts</p>
-            <ul className="flex gap-[15px]">
-              <li>
-                <Link to="tel:+12345678990" className="group flex items-center gap-[10px]">
-                  <Icon
-                    id="phone"
-                    className="h-[13px] w-[13px] fill-accentRedColor transition group-hover:fill-customTitleColor"
-                  />
-                  <h5 className="group-hover:text-customTitleColor">+1 234 567-89-90</h5>
-                </Link>
-              </li>
-              <li>
-                <Link to="mailto:info@sonomics.com" className="group flex items-center gap-[10px]">
-                  <Icon
-                    id="email"
-                    className="h-[14px] w-[15px] fill-accentRedColor transition group-hover:fill-customTitleColor"
-                  />
-                  <h5 className="group-hover:text-customTitleColor">info@sonomics.com</h5>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="https://www.google.com/maps?q=Barcelona,+Spain"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="group flex items-center gap-[10px]"
-                >
-                  <Icon
-                    id="location"
-                    className="h-[15px] w-[15px] fill-accentRedColor transition group-hover:fill-customTitleColor"
-                  />
-                  <h5 className="group-hover:text-customTitleColor">Barcelona, Spain</h5>
-                </Link>
-              </li>
+            <ul className="flex gap-[30px]">
+              <ul className="flex gap-[30px]">
+                {contacts.map(({ id, href, text, target, rel }) => (
+                  <li key={id}>
+                    <Link
+                      to={href}
+                      className="group flex items-center gap-[10px]"
+                      target={target}
+                      rel={rel}
+                    >
+                      <Icon
+                        id={id}
+                        className="group-hover:fill-themeAccentColor h-[15px] w-[15px] fill-accentRedColor transition"
+                      />
+                      <p className="bold-text group-hover:text-themeAccentColor">{text}</p>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </ul>
           </div>
           <div className="relative w-full overflow-hidden rounded-[40px]">
@@ -60,9 +41,9 @@ export const LetsTalk = () => {
               srcSet={`${commonImages.barcelona_1x} 1x, ${commonImages.barcelona_2x} 2x`}
               src={commonImages.barcelona_1x}
               alt="View of Barcelona"
-              width={619}
+              width={620}
               loading="lazy"
-              className="h-[266px] object-cover"
+              className="h-[420px] object-cover"
             />
           </div>
         </div>
