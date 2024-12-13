cacfb151c87ad705d4d6a453a3381a60d46f2543
Sergey Kabanets <skabanets97@gmail.com>
2024-12-13 23:08:08 +0200

    Add contacts constant

---
+++ src/constants/contacts.ts
@@ -0,0 +1,19 @@
+export const contacts = [
+  {
+    id: "phone",
+    href: "tel:+12345678990",
+    text: "+1 234 567-89-90",
+  },
+  {
+    id: "email",
+    href: "mailto:info@sonomics.com",
+    text: "info@sonomics.com",
+  },
+  {
+    id: "location",
+    href: "https://www.google.com/maps?q=Barcelona,+Spain",
+    text: "Barcelona, Spain",
+    target: "_blank",
+    rel: "noopener noreferrer",
+  },
+];
