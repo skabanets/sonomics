199c712d03c9099c9e99e5eff5402c93afbc5ce4
Sergey Kabanets <skabanets97@gmail.com>
2024-12-15 01:30:42 +0200

    Update IndustryCard component

--- src/components/IndustryCard.tsx
+++ src/components/IndustryCard.tsx
@@ -10,20 +10,18 @@ interface IndustryCardProps {
 
 export const IndustryCard = ({ industry }: IndustryCardProps) => {
   return (
-    <li className="rounded-[20px] border border-solid border-transparent bg-darkBgColor transition hover:border-accentYellowColor">
-      <Link to={industry.link} className="flex h-[317px] w-[340px] flex-col gap-[38px] p-[30px]">
-        <Icon id={industry.icon} className="size-10 fill-accentYellowColor" />
-        <div className="flex flex-col gap-[8px]">
-          <CustomTitle
-            type="h5"
-            wrapperStyles="flex items-center justify-between"
-            iconColor="fill-whiteTextColor"
-            titleStyles="text-whiteTextColor"
-            text={industry.name}
-          />
-          <p className="text-secondaryLightTextColor opacity-[0.6]">{industry.description}</p>
-        </div>
-      </Link>
-    </li>
+    <Link to={industry.link} className="flex h-[317px] w-[340px] flex-col gap-[38px] p-[30px]">
+      <Icon id={industry.icon} className="size-10 fill-accentYellowColor" />
+      <div className="flex flex-col gap-[8px]">
+        <CustomTitle
+          type="h5"
+          wrapperStyles="flex items-center justify-between"
+          iconColor="fill-whiteTextColor"
+          titleStyles="text-whiteTextColor"
+          text={industry.name}
+        />
+        <p className="text-secondaryLightTextColor opacity-[0.6]">{industry.description}</p>
+      </div>
+    </Link>
   );
 };
