0543e73d372da62b8ed36a48d16a16015c90bee0
Sergey Kabanets <skabanets97@gmail.com>
2024-12-15 23:13:42 +0200

    Update IndustryCard component

--- src/components/IndustryCard.tsx
+++ src/components/IndustryCard.tsx
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom";
 
-import { CustomTitle, Icon } from "../components";
+import { CustomTitle } from "../components";
 
 import { Industry } from "../types";
+import { images } from "../assets";
 
 interface IndustryCardProps {
   industry: Industry;
 }
 
 export const IndustryCard = ({ industry }: IndustryCardProps) => {
+  const { industriesImages } = images;
   return (
-    <Link to={industry.link} className="flex h-[317px] w-[340px] flex-col gap-[38px] p-[30px]">
-      <Icon id={industry.icon} className="size-10 fill-accentYellowColor" />
+    <Link to={industry.link} className="flex h-[346px] w-[340px] flex-col gap-[38px] p-[30px]">
+      <img
+        srcSet={`${industriesImages[industry.image]} 1x, ${industriesImages[industry.retinaImage]} 2x`}
+        src={industriesImages[industry.image]}
+        alt="Case study 1"
+        width={70}
+        loading="lazy"
+      />
       <div className="flex flex-col gap-[8px]">
         <CustomTitle
           type="h5"
@@ -20,7 +28,9 @@ export const IndustryCard = ({ industry }: IndustryCardProps) => {
           titleStyles="text-whiteTextColor"
           text={industry.name}
         />
-        <p className="text-secondaryLightTextColor opacity-[0.6]">{industry.description}</p>
+        <p className="small-text text-secondaryLightTextColor opacity-[0.6]">
+          {industry.description}
+        </p>
       </div>
     </Link>
   );
