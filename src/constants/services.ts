e7509f7ef3fd62c998dd7d80fa822d584317c5b5
Sergey Kabanets <skabanets97@gmail.com>
2024-12-21 22:26:38 +0200

    Change services constant

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -34,4 +34,51 @@ export const services = [
       "Pilot projects to test and validate ideas before full-scale implementation",
     ],
   },
+  {
+    id: 4,
+    name: "End-to-End Application Development",
+    cardTitle: "End-to-End application development",
+    link: "/services",
+    features: [
+      "Custom software development tailored to your business goals",
+      "Mobile and web application development",
+      "Maintenance and support for legacy systems",
+      "Ongoing updates and feature enhancements to keep your software relevant",
+    ],
+  },
+  {
+    id: 5,
+    name: "Artificial Intelligence (AI) Integration",
+    cardTitle: "Transform your business with artificial intelligence",
+    link: "/services",
+    features: [
+      "Leverage machine learning for predictive analytics",
+      "Automate repetitive processes to free up resources",
+      "Enhance decision-making through AI-driven insights",
+      "Implement AI-powered chatbots to streamline customer service",
+    ],
+  },
+  {
+    id: 6,
+    name: "Agile Methodologies",
+    cardTitle: "Deliverables in 2 weeks: experience our demo version",
+    link: "/services",
+    features: [
+      "Quick turnaround demo to showcase immediate value",
+      "Real-time feedback and customization based on your needs",
+      "Pilot projects to test and validate ideas before full-scale implementation",
+    ],
+  },
+  {
+    id: 7,
+    name: "End-to-End Application Development",
+    cardTitle: "End-to-End application development",
+    link: "/services",
+    features: [
+      "Custom software development tailored to your business goals",
+      "Mobile and web application development",
+      "Maintenance and support for legacy systems",
+      "Ongoing updates and feature enhancements to keep your software relevant",
+    ],
+  },
 ];
