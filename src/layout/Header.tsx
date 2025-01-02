67478d179b84aa08c0c15d1f10fa15ed1fd5567c
VasylPopaz <pafos1997@gmail.com>
2025-01-03 00:01:03 +0100

    Update Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,19 +1,22 @@
+import { useRef } from "react";
 import { Button, ContactForm, Logo, Modal, NavMenu, ThemeToggle } from "../components";
 
 import { useModal } from "../hooks";
 
 export const Header = () => {
   const [isOpen, toggleModal, handleClickOnBackdrop] = useModal();
+  const headerRef = useRef<HTMLDivElement | null>(null);
 
   return (
     <>
-      <header className="header pt-[40px] transition !duration-150">
+      <header ref={headerRef} className="header pt-[40px] transition !duration-150">
         <div className="container flex items-center">
           <nav className="relative z-10 flex w-full justify-between">
             <Logo applyThemeColor />
             <div className="flex items-center gap-[50px]">
               <ThemeToggle />
               <NavMenu
+                headerRef={headerRef}
                 listClass="flex gap-[50px] font-medium text-mainTextColor"
                 linkClass="hover:text-themeAccentColor focus-visible:text-themeAccentColor"
               />
