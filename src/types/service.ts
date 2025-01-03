d2788b229392e49a538aac8789e535e146520cdd
Sergey Kabanets <skabanets97@gmail.com>
2025-01-03 15:59:44 +0200

    Update Service type

--- src/types/service.ts
+++ src/types/service.ts
@@ -1,7 +1,78 @@
-export interface Service {
-  id: number;
+interface Hero {
+  image: string;
+  retinaImage: string;
+  text: string[];
+  boldText?: string;
+}
+
+interface Offers {
+  sectionTitle: string;
+  image: string;
+  retinaImage: string;
+  offersList: OfferDate[];
+}
+
+interface OfferDate {
+  offerName: string;
+  details: string[];
+  keyBenefits?: string[];
+}
+
+interface WhyChooseUs {
+  sectionTitle: string;
+  items: WhyChooseUsItem[];
+}
+
+interface WhyChooseUsItem {
+  title: string;
+  description: string;
+  image: string;
+  retinaImage: string;
+}
+
+interface Expertise {
+  text: string;
+  accentWords: string[];
+}
+
+interface Technologies {
+  sectionTitle: string;
+  cards: TechnologyCard[];
+  description?: string;
+}
+
+interface TechnologyCard {
+  technologyTitle: string;
+  technologyProperties: TechnologyData[];
+}
+
+interface TechnologyData {
   name: string;
-  cardTitle: string;
+  text: string | string[];
+}
+
+interface ApproachAndAdvantages {
+  sectionTitle: string;
+  items: ApproachAndAdvantagesItem[];
+}
+
+interface ApproachAndAdvantagesItem {
+  description: string | string[];
+  icon: string;
+  name?: string;
+}
+
+export interface Service {
+  id: string;
+  name: string;
+  title: string;
   link: string;
   features: string[];
+  hero: Hero;
+  offers: Offers;
+  whyChooseUs: WhyChooseUs;
+  caseStudyId: number;
+  expertise?: Expertise;
+  technologies?: Technologies;
+  approachAndAdvantages?: ApproachAndAdvantages;
 }
