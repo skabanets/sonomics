0d8910fb56d2e335f7e31136d7864994df0be43c
Sergey Kabanets <skabanets97@gmail.com>
2025-01-03 12:17:54 +0200

    Add service links to the routes constant

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -4,4 +4,11 @@ export enum routes {
   Industries = "/industries",
   CaseStudy = "/case-study",
   About = "/about",
+  EndToEndApplicationDevelopment = "/services/end-to-end-application-development",
+  ArtificialIntelligence = "/services/artificial-intelligence",
+  DemoVersion = "/services/demo-version",
+  RequirementAnalysisAndDevelopmentPlanning = "/services/requirement-analysis-and-development-planning",
+  AgileMethodologies = "/services/agile-methodologies",
+  OptimizeBusinessProcessesWithCRM = "/services/optimize-business-processes-with-crm",
+  CloudIntegration = "/services/cloud-integration",
 }
