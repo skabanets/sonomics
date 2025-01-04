7bb78c1de323a47907a0fdcc4f9ce5fb7e6c3f7d
VasylPopaz <pafos1997@gmail.com>
2025-01-04 15:05:58 +0100

    Add ServicesHero section

---
+++ src/sections/Services/common/ServicesHero.tsx
@@ -0,0 +1,73 @@
+import { useParams } from "react-router-dom";
+
+import { BackLink, Button, ContactForm, Modal } from "../../../components";
+
+import { useModal, useTheme } from "../../../hooks";
+import type { Hero } from "../../../types";
+import { routes, ThemeMode } from "../../../constants";
+import { images } from "../../../assets";
+
+interface ServicesHeroProps {
+  title: string;
+  hero: Hero;
+}
+
+export const ServicesHero = ({ title, hero }: ServicesHeroProps) => {
+  const { id } = useParams();
+
+  const { theme } = useTheme();
+
+  const [isOpen, toggleModal, handleClickOnBackdrop] = useModal();
+
+  const getImageWidth = () => {
+    if (id && id === routes.EndToEndApplicationDevelopment.slice(10)) {
+      return "540";
+    } else if (id === routes.CloudIntegration.slice(10)) {
+      return "470";
+    } else {
+      return "460";
+    }
+  };
+
+  const imageWidth = getImageWidth();
+
+  const {
+    servicesDetails: { heroImages },
+  } = images;
+  const { boldText, text, image, retinaImage } = hero;
+
+  return (
+    <>
+      <section className="pb-[80px] pt-[120px]">
+        <div className="container">
+          <BackLink to={routes.Services} label="Back to services" className="mb-[20px]" />
+          <h1 className="mb-[30px] max-w-[920px]">{title}</h1>
+          <div className="flex items-center justify-between">
+            <div className="max-w-[675px]">
+              {boldText && <h5 className="mb-[20px]">{boldText}</h5>}
+              {text.map((text, index) => (
+                <p key={index} className="big-text mb-[40px] last:mb-[20px]">
+                  {text}
+                </p>
+              ))}
+              <Button label="Let's discuss your project" width="w-[300px]" onClick={toggleModal} />
+            </div>
+            <img
+              srcSet={`${heroImages[image as keyof typeof heroImages]} 1x, ${heroImages[retinaImage as keyof typeof heroImages]} 2x`}
+              src={heroImages[image as keyof typeof heroImages]}
+              alt="Image"
+              width={imageWidth}
+              height="406"
+              className={`rounded-t-[20px] object-cover ${id !== routes.CloudIntegration.slice(10) ? "rounded-b-[20px]" : ""} ${theme === ThemeMode.LIGHT ? "brightness-[90%]" : ""}`}
+            />
+          </div>
+        </div>
+      </section>
+      {isOpen && (
+        <Modal {...{ toggleModal, handleClickOnBackdrop }}>
+          <ContactForm onClose={toggleModal} />
+        </Modal>
+      )}
+    </>
+  );
+};
