f1fbecc0bd03401d08d69a7454ea2d3250e875b1
VasylPopaz <pafos1997@gmail.com>
2025-01-04 15:04:18 +0100

    Update BackLink component

--- src/components/BackLink.tsx
+++ src/components/BackLink.tsx
@@ -2,12 +2,16 @@ import { Link, LinkProps } from "react-router-dom";
 
 interface BackLinkProps extends LinkProps {
   label: string;
+  className?: string;
 }
 
-export const BackLink = ({ label, ...rest }: BackLinkProps) => {
+export const BackLink = ({ label, className = "", ...rest }: BackLinkProps) => {
   return (
-    <Link {...rest}>
-      <h6>{label}</h6>
+    <Link
+      {...rest}
+      className={`inline-block text-secondaryTextColor hover:text-themeAccentColor focus-visible:text-themeAccentColor ${className}`}
+    >
+      <h6 className="underline">{label}</h6>
     </Link>
   );
 };
