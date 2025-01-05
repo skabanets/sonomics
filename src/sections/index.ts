4e89223bc1848d1c2cc018553bc058e0452c6b06
Sergey Kabanets <skabanets97@gmail.com>
2025-01-05 12:14:50 +0200

    Add reexports for common Services sections

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -3,14 +3,23 @@ export * from "./Home/WhyUs";
 export * from "./Home/Industries";
 export * from "./Home/Services";
 export * from "./Home/CaseStudies";
+
 export * from "./About/AboutSonomics";
 export * from "./About/OurMission";
 export * from "./About/SonomicsNumbers";
 export * from "./About/SuccessfulStories";
 export * from "./About/Team";
+
 export * from "./Services/OurServices";
 export * from "./Services/OurCoreServices";
 export * from "./Services/WhyChooseUs";
+export * from "./Services/common/ServicesHero";
+export * from "./Services/common/ServicesWhyChooseSonomics";
 export * from "./Services/common/ServicesCaseStudy";
 export * from "./Services/common/ServicesCaseStudyParallax";
+export * from "./Services/common/ServicesWhatWeOffer";
+export * from "./Services/common/ServicesExpertise";
+export * from "./Services/common/ServicesTechnologies";
+export * from "./Services/common/ServicesApproachAndAdvantages";
+
 export * from "./Common/LetsTalk";
