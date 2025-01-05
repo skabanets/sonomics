4e36cfac8f6141ad5949d580b902f0f9d02d9a15
Sergey Kabanets <skabanets97@gmail.com>
2025-01-05 11:17:18 +0200

    Add ServicesApproachAndAdvantages section

---
+++ src/sections/Services/common/ServicesApproachAndAdvantages.tsx
@@ -0,0 +1,112 @@
+import { motion } from "framer-motion";
+
+import { Icon } from "../../../components";
+
+import { routes, slideInWithFade } from "../../../constants";
+import { ApproachAndAdvantages } from "../../../types";
+
+interface ServicesApproachAndAdvantagesProps {
+  approachAndAdvantages: ApproachAndAdvantages;
+  id: string;
+}
+
+export const ServicesApproachAndAdvantages = ({
+  approachAndAdvantages,
+  id,
+}: ServicesApproachAndAdvantagesProps) => {
+  const { sectionTitle, items } = approachAndAdvantages;
+
+  const case1 = id === routes.RequirementAnalysisAndDevelopmentPlanning.slice(10);
+  const case2 = id === routes.AgileMethodologies.slice(10);
+  const case3 =
+    id === routes.OptimizeBusinessProcessesWithCRM.slice(10) ||
+    id === routes.CloudIntegration.slice(10);
+
+  if (case1) {
+    return (
+      <section className="py-[100px]">
+        <motion.div className="container flex justify-between" {...slideInWithFade}>
+          <h2 className="w-[430px]">{sectionTitle}</h2>
+          <ul className="grid grid-cols-2 gap-[40px]">
+            {items.map((item, index) => (
+              <li key={index}>
+                <div className="flex h-[390px] w-[370px] flex-col items-start gap-[30px] rounded-bl-[20px] rounded-br-[20px] border-t-2 border-solid border-themeAccentColor bg-letsTalkBgColor px-[30px] py-[40px] transition">
+                  <div className="flex items-center gap-[20px]">
+                    <Icon
+                      id={item.icon}
+                      className="size-[50px] fill-themeAccentColor stroke-themeAccentColor"
+                    />
+                    <h5 className="w-[240px]">{item.name}</h5>
+                  </div>
+                  {typeof item.description !== "string" && (
+                    <ul className="flex flex-col gap-[10px]">
+                      {item.description.map((text, index) => (
+                        <li key={index}>
+                          <p className="small-text text-secondaryTextColor">{text}</p>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      </section>
+    );
+  }
+
+  if (case2) {
+    return (
+      <section>
+        <motion.div className="container flex flex-col gap-[40px]" {...slideInWithFade}>
+          <h2>{sectionTitle}</h2>
+          <ul className="flex justify-between">
+            {items.map((item, index) => (
+              <li
+                key={index}
+                className="flex h-[260px] w-[300px] flex-col items-start gap-[20px] rounded-bl-[20px] rounded-br-[20px] border-t-2 border-solid border-themeAccentColor bg-letsTalkBgColor px-[30px] pt-[40px] transition"
+              >
+                <Icon
+                  id={item.icon}
+                  className="size-[60px] fill-themeAccentColor stroke-themeAccentColor"
+                />
+                <p className="big-text">{item.description}</p>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      </section>
+    );
+  }
+
+  if (case3) {
+    return (
+      <section className="py-[100px]">
+        <motion.div className="container relative flex justify-end" {...slideInWithFade}>
+          <h2 className="absolute left-0 top-0 w-[269px]">{sectionTitle}</h2>
+          <ul className="grid-card-container">
+            <li className="empty-cell"></li>
+            {items.map((item, index) => (
+              <li
+                key={index}
+                className={`flex h-[310px] w-[370px] flex-col items-start gap-[30px] rounded-bl-[20px] rounded-br-[20px] border-t-2 border-solid border-themeAccentColor bg-letsTalkBgColor px-[30px] py-[40px] transition item-${index + 1}`}
+              >
+                <Icon
+                  id={item.icon}
+                  className="size-[60px] fill-themeAccentColor stroke-themeAccentColor"
+                />
+                <div className="flex flex-col gap-[10px]">
+                  <h4>{item.name}</h4>
+                  <p className="small-text text-secondaryTextColor">{item.description}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      </section>
+    );
+  }
+
+  return null;
+};
