770a909aac876ee304a490351cf516ea7687ca3d
Sergey Kabanets <skabanets97@gmail.com>
2025-01-06 16:37:27 +0200

    Update animationProps constant

--- src/constants/animationProps.ts
+++ src/constants/animationProps.ts
@@ -11,3 +11,25 @@ export const opacityFade = {
   transition: { duration: 2, ease: "easeOut", delay: 0.25 },
   viewport: { once: true, amount: 0.1 },
 };
+
+export const industriesContainerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+export const cardVariants = {
+  hidden: { opacity: 0, x: 100 },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.8,
+      ease: "easeInOut",
+    },
+  },
+};
