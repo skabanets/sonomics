5d445d2e009c543e7f875c8a9549da0568557983
Sergey Kabanets <skabanets97@gmail.com>
2025-01-11 00:27:09 +0200

    Update routes and caseStudies constants

--- src/constants/caseStudies.ts
+++ src/constants/caseStudies.ts
@@ -1,3 +1,4 @@
+import { routes } from "../constants";
 import { images } from "../assets";
 
 const { casesImages } = images;
@@ -5,8 +6,8 @@ const { casesImages } = images;
 export const caseStudies = [
   {
     id: 1,
-    link: "/case-study",
-    name: "Case study name",
+    link: routes.FintechСaseStudy,
+    name: "Fintech transformation success",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
     image: "case1_1x",
@@ -16,7 +17,7 @@ export const caseStudies = [
   },
   {
     id: 2,
-    link: "/case-study",
+    link: routes.FintechСaseStudy,
     name: "Case study name",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
@@ -27,7 +28,7 @@ export const caseStudies = [
   },
   {
     id: 3,
-    link: "/case-study",
+    link: routes.FintechСaseStudy,
     name: "Case study name",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
@@ -38,7 +39,29 @@ export const caseStudies = [
   },
   {
     id: 4,
-    link: "/case-study",
+    link: routes.FintechСaseStudy,
+    name: "Case study name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+    image: "case2_1x",
+    retinaImage: "case2_2x",
+    bgImage1x: casesImages.case_lg_1_1x,
+    bgImage2x: casesImages.case_lg_1_2x,
+  },
+  {
+    id: 5,
+    link: routes.FintechСaseStudy,
+    name: "Case study name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
+    image: "case1_1x",
+    retinaImage: "case1_2x",
+    bgImage1x: casesImages.case_lg_1_1x,
+    bgImage2x: casesImages.case_lg_1_2x,
+  },
+  {
+    id: 6,
+    link: routes.FintechСaseStudy,
     name: "Case study name",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
