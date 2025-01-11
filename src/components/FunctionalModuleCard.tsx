cef12de9d52484d66daff6ee7440db60ef802762
Sergey Kabanets <skabanets97@gmail.com>
2025-01-11 22:40:41 +0200

    Add FunctionalModuleCard component

---
+++ src/components/FunctionalModuleCard.tsx
@@ -0,0 +1,28 @@
+//TODO: Extract ModuleItem type into a separate file
+
+interface ModuleItem {
+  moduleName: string;
+  details: string[];
+}
+
+interface FunctionalModuleCardProps {
+  module: ModuleItem;
+}
+
+export const FunctionalModuleCard = ({ module }: FunctionalModuleCardProps) => {
+  return (
+    <div className="flex h-[318px] w-[410px] flex-col gap-[20px] rounded-[10px] border border-solid border-blackBgColor bg-formFieldBgColor p-[30px]">
+      <h4>{module.moduleName}</h4>
+      <ul className="flex flex-col gap-[10px]">
+        {module.details.map((text, index) => (
+          <li
+            key={index}
+            className="relative pl-[16px] before:absolute before:left-0 before:top-[10px] before:h-[6px] before:w-[6px] before:rounded-full before:bg-accentYellowColor"
+          >
+            <p className="small-text">{text}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
