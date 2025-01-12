6b9ae267576d4dfc1547a9d511f1300313cf6815
VasylPopaz <pafos1997@gmail.com>
2025-01-12 16:02:00 +0100

    Add useBreakpointValue hook

---
+++ src/hooks/useBreakpointValue.ts
@@ -0,0 +1,15 @@
+import { useMediaQuery } from "react-responsive";
+
+type BreakpointValue<T> = [T, T, T]; // Значения для [desktop, tablet, mobile]
+
+export const useBreakpointValue = <T>(values: BreakpointValue<T>): T => {
+  const isDesktop = useMediaQuery({ minWidth: 1440 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
+  const isMobile = useMediaQuery({ maxWidth: 767.98 });
+
+  if (isDesktop) return values[0];
+  if (isTablet) return values[1];
+  if (isMobile) return values[2];
+
+  return values[2];
+};
