b1c219e88f4df6c557405307056c5717fde64249
Sergey Kabanets <skabanets97@gmail.com>
2025-01-17 00:11:51 +0200

    Add animation for IndustryHero section

--- src/sections/Industries/IndustryHero.tsx
+++ src/sections/Industries/IndustryHero.tsx
@@ -1,12 +1,16 @@
+import { motion } from "framer-motion";
+
+import { slideInWithFade } from "../../constants";
+
 export const IndustryHero = () => {
   return (
     <section className="pb-[40px] pt-[60px]">
-      <div className="container">
+      <motion.div className="container" {...slideInWithFade}>
         <h1 className="max-w-[1200px]">
           We deliver <span className="text-themeAccentColor">real value</span> to clients because of
           our knowledge of the industries in which they operate
         </h1>
-      </div>
+      </motion.div>
     </section>
   );
 };
