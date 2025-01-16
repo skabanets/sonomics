bcecc13d66fd378b83ed99e668e788427665d4b2
VasylPopaz <pafos1997@gmail.com>
2025-01-16 21:23:01 +0100

    Update industries constant

--- src/constants/industries.ts
+++ src/constants/industries.ts
@@ -1,29 +1,187 @@
 export const industries = [
   {
-    id: 1,
+    name: "Telecommunications",
+    description: [
+      "Empowering telcos in their journey to TechCos – building future-ready intelligent connectivity solutions",
+      "Designing next-generation, AI-driven products and interconnected solutions that adapt to the evolving digital ecosystem and facilitate the expansion new use cases.",
+    ],
+    link: "/industries",
+    technologies: {
+      title: "Technologies we use in telecommunications",
+      description:
+        "Designing next-generation, AI-driven products and interconnected solutions that adapt to the evolving digital ecosystem and facilitate the expansion new use cases.",
+    },
+    services: {
+      title: "Services we provide",
+      description:
+        "Designing next-generation, AI-driven products and interconnected solutions that adapt to the evolving digital ecosystem and facilitate the expansion new use cases.",
+    },
+    caseStudy: {
+      title: "Revolution in telecommunications",
+      description:
+        "The solution aimed to provide intuitive access to ISP services and drive informed decision-making.",
+      link: "/case-study",
+    },
+    images: {
+      image: "telecommunications_1x",
+      retinaImage: "telecommunications_2x",
+      icon: "",
+      retinaIcon: "",
+    },
+    buttonText: "Start your project",
+  },
+  {
+    name: "Media research",
+    description: [
+      "We combine our technology and industry expertise to design, build, and deliver leading technology solutions for digital media, music, sports, video, art, and publishing organizations.",
+    ],
+    link: "/industries",
+    technologies: {
+      title: "Technologies we use in media research",
+      description:
+        "We combine our technology and industry expertise to design, build, and deliver leading technology solutions for digital media, music, sports, video, art, and publishing organizations.",
+    },
+    services: {
+      title: "Services we provide",
+      description:
+        "We combine our technology and industry expertise to design, build, and deliver leading technology solutions for digital media, music, sports, video, art, and publishing organizations.",
+    },
+    caseStudy: {
+      title: "Optimizing media research",
+      description: "Increase efficiency by providing a unified approach to content distribution",
+      link: "/case-study",
+    },
+    images: {
+      image: "media_research_1x",
+      retinaImage: "media_research_2x",
+      icon: "",
+      retinaIcon: "",
+    },
+    buttonText: "Start your fintech project",
+  },
+  {
     name: "Fintech",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
-    image: "fintech_1x",
-    retinaImage: "fintech_2x",
+    description: [
+      "We work across all areas of financial services and capital markets, offering engineering services with domain expertise, proven by the CFA certification.",
+      "Our clients include banks, exchanges, hedge funds, market data providers, rating agencies, market utilities, insurance companies, fintech firms, and inter-dealer brokers.",
+    ],
     link: "/industries",
+    technologies: {
+      title: "Technologies we use in fintech",
+      description:
+        "We work across all areas of financial services and capital markets, offering engineering services with domain expertise, proven by the CFA certification.",
+    },
+    services: {
+      title: "Services we provide",
+      description:
+        "We work across all areas of financial services and capital markets, offering engineering services with domain expertise, proven by the CFA certification.",
+    },
+    caseStudy: {
+      title: "Fintech transformation success",
+      description:
+        "A leading financial services company wanted to upgrade its platform to stay competitive.",
+      link: "/case-study",
+    },
+    images: {
+      image: "fintech_1x",
+      retinaImage: "fintech_2x",
+      icon: "fintech_icon_1x",
+      retinaIcon: "fintech_icon_2x",
+    },
+    buttonText: "Start your fintech project",
   },
   {
-    id: 2,
-    name: "Ecommerce",
-    description:
-      "Ut enim ad minim veniam, quis nostrud. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
-    image: "ecommerce_1x",
-    retinaImage: "ecommerce_2x",
+    name: "E-commerce",
+    description: [
+      "Retailers are looking for new ways to reach consumers, create better products, personalize experiences and cultivate customers loyalty.",
+      "Sonomics has been helping retailers balance short-term results with long-term digital improvement, boost their operational efficiency, and increase revenue growth through advanced technology.",
+    ],
     link: "/industries",
+    technologies: {
+      title: "Technologies we use in e-commerce",
+      description:
+        "Sonomics has been helping retailers balance short-term results with long-term digital improvement, boost their operational efficiency, and increase revenue growth through advanced technology.",
+    },
+    services: {
+      title: "Services we provide",
+      description:
+        "Sonomics has been helping retailers balance short-term results with long-term digital improvement, boost their operational efficiency, and increase revenue growth through advanced technology.",
+    },
+    caseStudy: {
+      title: "Transforming E-commerce",
+      description:
+        "The client's goal was to modernize their system, and deliver an exceptional shopping experience to a global customer base",
+      link: "/case-study",
+    },
+    images: {
+      image: "ecommerce_1x",
+      retinaImage: "ecommerce_2x",
+      icon: "ecommerce_icon_1x",
+      retinaIcon: "ecommerce_icon_2x",
+    },
+    buttonText: "Start your fintech project",
   },
   {
-    id: 3,
     name: "Cryptocurrency",
-    description:
-      "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
-    image: "crypto_1x",
-    retinaImage: "crypto_2x",
+    description: [
+      "We work across all areas of financial services and capital markets, offering engineering services with domain expertise, proven by the CFA certification.",
+      "Our clients include banks, exchanges, hedge funds, market data providers, rating agencies, market utilities, insurance companies, fintech firms, and inter-dealer brokers.",
+    ],
     link: "/industries",
+    technologies: {
+      title: "Technologies we use in cryptocurrency",
+      description:
+        "We work across all areas of financial services and capital markets, offering engineering services with domain expertise, proven by the CFA certification.",
+    },
+    services: {
+      title: "Services we provide",
+      description:
+        "We work across all areas of financial services and capital markets, offering engineering services with domain expertise, proven by the CFA certification.",
+    },
+    caseStudy: {
+      title: "Advancing crypto innovation",
+      description:
+        "A leading financial services company wanted to upgrade its platform to stay competitive.",
+      link: "/case-study",
+    },
+    images: {
+      image: "crypto_1x",
+      retinaImage: "crypto_2x",
+      icon: "crypto_icon_1x",
+      retinaIcon: "crypto_icon_2x",
+    },
+    buttonText: "Start your fintech project",
+  },
+  {
+    name: "Healthcare",
+    description: [
+      "Technology has the power to not only elevate the standard of living but also save precious human lives. Implementing advanced solutions like Data Analytics, Artificial Intelligence (AI), and Cloud Computing in healthcare comes with challenges, including ensuring data privacy, security, regulatory compliance, and cost efficiency.",
+    ],
+    link: "/industries",
+    technologies: {
+      title: "Technologies we use in hospitality",
+      description:
+        "Technology has the power to not only elevate the standard of living but also save precious human lives. Implementing advanced solutions like Data Analytics.",
+    },
+    services: {
+      title: "Services we provide",
+      description:
+        "Artificial Intelligence (AI), and Cloud Computing in healthcare comes with challenges, including ensuring data privacy, security, regulatory compliance, and cost efficiency.",
+    },
+    caseStudy: {
+      title: "Pharmaceutical distribution",
+      description:
+        "The company collaborates with manufacturers, healthcare providers, and government agencies",
+      link: "/case-study",
+    },
+    images: {
+      image: "health_care_1x",
+      retinaImage: "health_care_2x",
+      icon: "",
+      retinaIcon: "",
+    },
+    buttonText: "Start your fintech project",
   },
 ];
+
+export const homeIndustries = [industries[2], industries[3], industries[4]];
