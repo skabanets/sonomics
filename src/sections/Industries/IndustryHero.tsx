302e1bb46ecf786668a3d787f1ac2149948df15f
VasylPopaz <pafos1997@gmail.com>
2025-01-16 17:22:31 +0100

    Add IndustryHero section

---
+++ src/sections/Industries/IndustryHero.tsx
@@ -0,0 +1,12 @@
+export const IndustryHero = () => {
+  return (
+    <section className="pb-[40px] pt-[60px]">
+      <div className="container">
+        <h1 className="max-w-[1200px]">
+          We deliver <span className="text-themeAccentColor">real value</span> to clients because of
+          our knowledge of the industries in which they operate
+        </h1>
+      </div>
+    </section>
+  );
+};
