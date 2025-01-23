cba022c78a5ce6e783f328ff9afc751b5e986e74
VasylPopaz <pafos1997@gmail.com>
2025-01-24 00:26:31 +0100

    Update Footer styles for mobile layout

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -7,22 +7,24 @@ import { socialLinks } from "../constants";
 export const Footer = () => {
   return (
     <footer className="footer bg-blackBgColor pb-[40px] pt-[80px]">
-      <div className="full-hd-container flex pb-[40px]">
-        <div className="mr-auto w-[281px] space-y-[22px]">
+      <div className="full-hd-container pb-[30px] md:flex md:pb-[40px]">
+        <div className="mb-[30px] flex flex-wrap space-y-[20px] md:mb-0 md:mr-auto md:w-[281px] md:space-y-[22px]">
           {" "}
           <Logo className="fill-whiteTextColor" />
-          <h2 className="text-whiteTextColor">Let&#8217;s work together</h2>
+          <h2 className="text-[34px] leading-[48px] text-whiteTextColor">
+            Let&#8217;s work together
+          </h2>
         </div>
-        <nav className="mr-[130px]">
+        <nav className="mr-[120px] md:mr-[130px]">
           <NavMenu
-            listClass="flex flex-col gap-[30px] text-whiteTextColor"
-            linkClass="hover:text-accentYellowColor focus-visible:text-accentYellowColor"
+            listClass="flex w-full flex-wrap h-[106px] gap-x-[70px] gap-y-[42px] mb-[30px] md:mb-0 md:h-auto flex-col md:gap-[30px] text-whiteTextColor"
+            linkClass="md:w-auto hover:text-accentYellowColor focus-visible:text-accentYellowColor"
           />
         </nav>
         <div className="flex flex-col justify-between">
-          <address className="not-italic">
+          <address className="mb-[40px] not-italic md:mb-0">
             <p className="label mb-[15px] text-secondaryLightTextColor">Contact us</p>
-            <ul className="bold-text flex flex-col gap-[15px] text-whiteTextColor">
+            <ul className="bold-text flex flex-row gap-[50px] text-whiteTextColor md:flex-col md:gap-[15px]">
               <li>
                 <Link
                   to="tel:+12345678990"
@@ -57,8 +59,10 @@ export const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="full-hd-container relative left-0 flex items-center justify-between pt-[20px] before:absolute before:top-0 before:h-[1px] before:w-[calc(100vw-160px)] before:max-w-[1760px] before:bg-grey600Color before:content-['']">
-        <p className="small-text text-white">&copy; 2025 Sonomics, Inc. All rights reserved</p>
+      <div className="full-hd-container relative left-0 flex items-center justify-between pt-[30px] before:absolute before:top-0 before:h-[1px] before:w-[360px] before:max-w-[1760px] before:bg-grey600Color before:content-[''] md:pt-[20px] md:before:w-[calc(100vw-160px)]">
+        <p className="small-text w-[186px] text-white md:w-auto">
+          &copy; 2025 Sonomics, Inc. All rights reserved
+        </p>
         <div className="small-text flex items-center justify-end gap-[15px] text-whiteTextColor">
           <span> To top </span>
           <ScrollUpButton />
