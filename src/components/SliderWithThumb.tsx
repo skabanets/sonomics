7e7f8dae7d55f20ce09ae1a5cdaa152cdc0bc614
VasylPopaz <pafos1997@gmail.com>
2025-01-23 11:45:50 +0100

    Add dnd for Team slider

--- src/components/SliderWithThumb.tsx
+++ src/components/SliderWithThumb.tsx
@@ -8,6 +8,7 @@ interface SliderWithThumbProps {
   containerWidth: number;
   cardWidth: number;
   gap: number;
+  sliderValues: { translateValue: number; totalWidth: number };
   onChange: (values: { translateValue: number; totalWidth: number }) => void;
 }
 
@@ -16,30 +17,29 @@ export const SliderWithThumb = ({
   containerWidth,
   cardWidth,
   gap,
+  sliderValues,
   onChange,
 }: SliderWithThumbProps) => {
   const totalWidth = itemsCount * cardWidth + (itemsCount - 1) * gap;
   const maxTranslateValue = Math.max(totalWidth - containerWidth, 0);
 
-  const [translateValue, setTranslateValue] = useState(0);
   const [iconName, setIconName] = useState("drag-hand");
 
   const handleSliderChange = (value: number) => {
     const newTranslateValue = (maxTranslateValue * value) / 100;
-    setTranslateValue(newTranslateValue);
 
     if (onChange) {
       onChange({ translateValue: newTranslateValue, totalWidth });
     }
   };
-
+  const value = (sliderValues.translateValue / maxTranslateValue) * 100;
   return (
     <div className="group" tabIndex={0}>
       <ReactSlider
         className="relative h-[49px] cursor-pointer"
         thumbClassName="react-slider-thumb top-1/2 translate-y-[-50%] w-[49px] h-[49px] flex items-center justify-center bg-darkBgColor rounded-full border border-accentYellowColor outline-none group-hover:h-[59px] group-hover:w-[59px] group-focus-visible:h-[59px] group-focus-visible:w-[59px] cursor-pointer"
         trackClassName="absolute top-1/2 left-0 translate-y-[-50%] h-[3px] rounded-[70px] bg-secondaryDarkBgColor group-hover:bg-accentYellowColor group-focus-visible:bg-accentYellowColor transition"
-        value={(translateValue / maxTranslateValue) * 100 || 0}
+        value={value}
         onChange={handleSliderChange}
         onBeforeChange={() => setIconName("drag-hand-active")}
         onAfterChange={() => setIconName("drag-hand")}
