2f509e7fb6f8e031209e8e9de5aa872b8e7fa167
Sergey Kabanets <skabanets97@gmail.com>
2025-01-24 03:57:02 +0200

    Update Service and CaseStudy types

--- src/types/case-study.ts
+++ src/types/case-study.ts
@@ -1,9 +1,58 @@
 import { images } from "../assets";
 
+export interface HeroImage {
+  image: string;
+  retinaImage: string;
+}
+
+export interface Overview {
+  client: string;
+  technologies: string;
+  description: string;
+  image: string;
+  retinaImage: string;
+}
+
+interface CaseStudyServiceType {
+  name: string;
+  description: string;
+  link: string;
+  image: string;
+  retinaImage: string;
+}
+
+export interface CaseStudyServicesType {
+  text?: string;
+  servicesData: CaseStudyServiceType[];
+}
+
+export interface FunctionalModule {
+  moduleName: string;
+  details: string[];
+}
+
+export interface OrdinaryItem {
+  title: string;
+  description: string;
+}
+
+export interface Achievements {
+  items: OrdinaryItem[];
+  image: string;
+  retinaImage: string;
+}
+
 export interface CaseStudy {
-  id: number;
+  id: string;
   link: string;
   name: string;
+  heroImages: HeroImage[];
+  overview: Overview;
+  challenges: string[];
+  services?: CaseStudyServicesType;
+  functionalModules?: FunctionalModule[];
+  technologies: OrdinaryItem[];
+  achievements: Achievements;
   description: string;
   image: keyof typeof images.casesImages;
   retinaImage: keyof typeof images.casesImages;
