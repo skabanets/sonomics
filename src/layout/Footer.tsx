418af89a761ec943ee3efe0fd24ecdd884f885b8
VasylPopaz <pafos1997@gmail.com>
2025-01-24 22:52:05 +0100

    Add Footer and Header styles for 320px screens

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -15,16 +15,16 @@ export const Footer = () => {
             Let&#8217;s work together
           </h2>
         </div>
-        <nav className="mr-[120px] md:mr-[130px]">
+        <nav className="mr-[120px] sm-max:mr-0 md:mr-[130px]">
           <NavMenu
-            listClass="flex w-full flex-wrap h-[106px] gap-x-[70px] gap-y-[40px] mb-[30px] md:mb-0 md:h-auto flex-col md:gap-[30px] text-whiteTextColor"
+            listClass=" flex w-full flex-wrap h-[106px] gap-x-[70px] gap-y-[40px] mb-[30px] md:mb-0 md:h-auto flex-col md:gap-[30px] text-whiteTextColor"
             linkClass="md:w-auto hover:text-accentYellowColor focus-visible:text-accentYellowColor"
           />
         </nav>
         <div className="flex flex-col justify-between">
-          <address className="mb-[40px] not-italic md:mb-0">
+          <address className="mb-[40px] not-italic sm-max:mb-[30px] md:mb-0">
             <p className="label mb-[15px] text-secondaryLightTextColor">Contact us</p>
-            <ul className="bold-text flex flex-row gap-[50px] text-whiteTextColor md:flex-col md:gap-[15px]">
+            <ul className="bold-text flex flex-row gap-[50px] text-whiteTextColor sm-max:flex-wrap sm-max:gap-[15px] md:flex-col md:gap-[15px]">
               <li>
                 <Link
                   to="tel:+12345678990"
@@ -50,7 +50,7 @@ export const Footer = () => {
                   to={link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex h-[40px] w-[112px] items-center justify-center rounded-[90px] border border-accentRedColor px-[25px] py-[10px] text-[12px] text-whiteTextColor transition hover:bg-accentRedColor focus-visible:bg-accentRedColor active:bg-accentRedColor"
+                  className="flex h-[40px] w-[112px] items-center justify-center rounded-[90px] border border-accentRedColor px-[25px] py-[10px] text-[12px] text-whiteTextColor transition hover:bg-accentRedColor focus-visible:bg-accentRedColor active:bg-accentRedColor sm-max:w-[calc((100vw-58px)/3)] sm-max:px-[12px]"
                 >
                   {text}
                 </Link>
@@ -59,11 +59,11 @@ export const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="full-hd-container relative left-0 flex items-center justify-between pt-[30px] before:absolute before:top-0 before:h-[1px] before:w-[360px] before:max-w-[1760px] before:bg-grey600Color before:content-[''] md:pt-[20px] md:before:w-[calc(100vw-160px)]">
-        <p className="small-text w-[186px] text-white md:w-auto">
-          &copy; 2025 Sonomics, Inc. All rights reserved
+      <div className="full-hd-container relative left-0 flex items-center justify-between pt-[30px] before:absolute before:top-0 before:h-[1px] before:w-[calc(100vw-30px)] before:max-w-[360px] before:bg-grey600Color before:content-[''] md:pt-[20px] md:before:w-[calc(100vw-160px)] lg:before:max-w-[1760px]">
+        <p className="small-text w-[186px] text-white sm-max:text-[14px] md:w-auto">
+          &copy; 2025 Sonomics, Inc. All&nbsp;rights reserved
         </p>
-        <div className="small-text flex items-center justify-end gap-[15px] text-whiteTextColor">
+        <div className="small-text flex items-center justify-end gap-[15px] text-whiteTextColor sm-max:text-[14px]">
           <span> To top </span>
           <ScrollUpButton />
         </div>
