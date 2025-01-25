64e25fdb4dbe63c0a8fb7aed4037b43acb91b57b
VasylPopaz <pafos1997@gmail.com>
2025-01-25 16:35:54 +0100

    Add mobile styles for the AboutSonomics section

--- src/sections/About/AboutSonomics.tsx
+++ src/sections/About/AboutSonomics.tsx
@@ -4,9 +4,9 @@ import { slideInWithFade } from "../../constants";
 
 export const AboutSonomics = () => {
   return (
-    <section className="pb-[80px] pt-[100px]">
+    <section className="pb-[20px] pt-[30px] md:pb-[80px] md:pt-[100px]">
       <motion.div className="container" {...slideInWithFade}>
-        <h6 className="mb-[20px] text-secondaryTextColor">About Sonomics</h6>
+        <h6 className="mb-[10px] text-secondaryTextColor md:mb-[20px]">About Sonomics</h6>
         <h1>
           We&#8217;re <span className="text-accentYellowColor"> driving </span> your
           <span className="text-accentYellowColor"> business</span> growth, ensuring stability and
