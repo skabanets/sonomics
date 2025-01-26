2bc0e832b4830310200a1dfff0f0268cb7881c35
VasylPopaz <pafos1997@gmail.com>
2025-01-26 13:53:32 +0100

    resolved issue with header appearance on scroll at SonomicsNumbers

--- src/sections/About/SonomicsNumbers.tsx
+++ src/sections/About/SonomicsNumbers.tsx
@@ -1,38 +1,61 @@
 import { useEffect, useRef, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import { motion } from "framer-motion";
+import throttle from "lodash.throttle";
 
 import { AnimatedNumber, Divider } from "../../components";
 
 import { opacityFade, sonomicsNumbers } from "../../constants";
 
 export const SonomicsNumbers = () => {
+  const [scrollDirection, setScrollDirection] = useState<"up" | "down">("down");
   const [isVisibleLastItem, setIsVisibleLastItem] = useState(false);
-  const lastItemRef = useRef(null);
+  const lastItemRef = useRef<HTMLLIElement | null>(null);
+
+  const isMobile = useMediaQuery({ maxWidth: 767.98 });
 
   useEffect(() => {
-    const itemRef = lastItemRef.current;
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = throttle(() => {
+      const currentScrollY = window.scrollY;
+      setScrollDirection(currentScrollY > lastScrollY ? "down" : "up");
+      lastScrollY = currentScrollY;
+    }, 300);
+
     const observer = new IntersectionObserver(
       ([entry]) => {
-        setIsVisibleLastItem(entry.isIntersecting);
+        const isIntersecting = entry.isIntersecting;
+
+        if (scrollDirection === "down" && isIntersecting) {
+          setIsVisibleLastItem(true);
+        } else if (scrollDirection === "up" && isIntersecting && entry.intersectionRatio === 1) {
+          setIsVisibleLastItem(false);
+        }
       },
-      { threshold: 0.1 }
+      { threshold: 1 }
     );
+
+    const itemRef = lastItemRef.current;
 
     if (itemRef) {
       observer.observe(itemRef);
     }
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
       if (itemRef) {
         observer.unobserve(itemRef);
       }
+      window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [scrollDirection]);
+
   return (
     <section className="py-[50px] md:py-[100px]">
       <div className="container">
         <motion.h2
-          className={`sticky top-[49px] z-10 before:absolute before:left-0 before:top-[-50px] before:z-[-1] before:block before:h-[140px] before:w-full before:bg-primaryBgColor md:top-[100px] md:w-[300px] md:pt-[20px] md:after:block md:after:h-[500px] ${isVisibleLastItem ? "after:block after:h-[620px]" : ""}`}
+          className={`sticky top-[49px] z-10 before:absolute before:left-0 before:top-[-50px] before:z-[-1] before:block before:h-[140px] before:w-full before:bg-primaryBgColor md:top-[100px] md:w-[300px] md:pt-[20px] md:after:block md:after:h-[500px] ${isVisibleLastItem && isMobile ? "-translate-y-[150%]" : ""}`}
           {...opacityFade}
         >
           Sonomics in numbers
@@ -54,7 +77,7 @@ export const SonomicsNumbers = () => {
                   finalNumber={number}
                   className="text-[120px] font-medium leading-[156px] text-accentRedColor"
                 />
-                <h4 className="w-[490px]">{title}</h4>
+                <h4 className="md:w-[490px]">{title}</h4>
 
                 {index < sonomicsNumbers.length - 1 && <Divider />}
               </motion.li>
