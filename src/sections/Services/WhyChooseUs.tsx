b74be595da484d3856be8a7f89475e4f0a82b806
Sergey Kabanets <skabanets97@gmail.com>
2025-01-27 22:55:17 +0200

    Update WhyChooseUs section

--- src/sections/Services/WhyChooseUs.tsx
+++ src/sections/Services/WhyChooseUs.tsx
@@ -1,31 +1,37 @@
 import { motion } from "framer-motion";
 
 import { images } from "../../assets";
-import { slideInWithFade, whyChooseUsPoints } from "../../constants";
+import { useTheme } from "../../hooks";
+import { ThemeMode, slideInWithFade, whyChooseUsPoints } from "../../constants";
 
 export const WhyChooseUs = () => {
   const { whyChooseUsImages } = images;
+  const { theme } = useTheme();
 
   return (
-    <section className="bg-secondaryBgColor py-[100px]">
+    <section className="bg-secondaryBgColor pb-[130px] pt-[100px]">
       <motion.div className="container" {...slideInWithFade}>
         <h2 className="mb-[60px]">Why choose us</h2>
         <ul className="flex justify-between">
-          {whyChooseUsPoints.map((item, index) => (
-            <li key={index} className="w-[250px]">
-              <img
-                srcSet={`${whyChooseUsImages[item.images[0] as keyof typeof whyChooseUsImages]} 1x, ${whyChooseUsImages[item.images[1] as keyof typeof whyChooseUsImages]} 2x`}
-                src={whyChooseUsImages[item.images[0] as keyof typeof whyChooseUsImages]}
-                alt={item.title}
-                width="200"
-                height="200"
-                loading="lazy"
-                className="mb-[40px] h-[200px] object-cover"
-              />
-              <h4 className="mb-[10px]">{item.title}</h4>
-              <p className="small-text text-secondaryTextColor">{item.description}</p>
-            </li>
-          ))}
+          {whyChooseUsPoints.map((item, index) => {
+            const imageTheme = theme === ThemeMode.DARK ? item.images.dark : item.images.light;
+
+            return (
+              <li key={index} className="w-[250px]">
+                <img
+                  srcSet={`${whyChooseUsImages[imageTheme[0] as keyof typeof whyChooseUsImages]} 1x, ${whyChooseUsImages[imageTheme[1] as keyof typeof whyChooseUsImages]} 2x`}
+                  src={whyChooseUsImages[imageTheme[0] as keyof typeof whyChooseUsImages]}
+                  alt={item.title}
+                  width={item.images.imageWidth}
+                  height="170"
+                  loading="lazy"
+                  className="mb-[40px] h-[170px] object-cover"
+                />
+                <h4 className="mb-[10px]">{item.title}</h4>
+                <p className="small-text text-secondaryTextColor">{item.description}</p>
+              </li>
+            );
+          })}
         </ul>
       </motion.div>
     </section>
