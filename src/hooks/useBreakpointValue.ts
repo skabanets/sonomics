4a3b2723356a762c854cc87dda6ef001ac17c247
VasylPopaz <pafos1997@gmail.com>
2025-01-28 14:10:19 +0100

    Update useBreakpointValue hook

--- src/hooks/useBreakpointValue.ts
+++ src/hooks/useBreakpointValue.ts
@@ -4,7 +4,7 @@ type BreakpointValue<T> = [T, T, T];
 
 export const useBreakpointValue = <T>(values: BreakpointValue<T>): T => {
   const isDesktop = useMediaQuery({ minWidth: 1440 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439.98 });
   const isMobile = useMediaQuery({ maxWidth: 767.98 });
 
   if (isDesktop) return values[0];
