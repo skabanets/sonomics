8cf0cf9f858793a1922e9b34d4c78a2114f6b6ef
Sergey Kabanets <skabanets97@gmail.com>
2025-01-28 23:58:33 +0200

    Add useMenuVisibility hook

---
+++ src/hooks/useMenuVisibility.ts
@@ -0,0 +1,57 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useMenuVisibility = (industriesLength: number, currentIndex: number) => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const sectionRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const isUserScrolling = useRef(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (isUserScrolling.current) return;
+
+        let firstSectionVisible = false;
+        let lastSectionVisible = false;
+
+        let maxVisibleRatio = 0;
+        let maxVisibleIndex = currentIndex;
+
+        entries.forEach((entry) => {
+          const index = sectionRefs.current.indexOf(entry.target as HTMLDivElement);
+
+          if (index === 0) {
+            firstSectionVisible = entry.isIntersecting && entry.intersectionRatio > 0.6;
+          }
+
+          if (index === sectionRefs.current.length - 1) {
+            lastSectionVisible = entry.isIntersecting && entry.intersectionRatio > 0.6;
+          }
+
+          if (entry.isIntersecting && entry.intersectionRatio > maxVisibleRatio) {
+            maxVisibleRatio = entry.intersectionRatio;
+            maxVisibleIndex = index;
+          }
+        });
+
+        setIsMenuVisible(
+          firstSectionVisible ||
+            lastSectionVisible ||
+            (maxVisibleIndex > 0 && maxVisibleIndex < sectionRefs.current.length - 1)
+        );
+      },
+      {
+        threshold: [0.1, 0.25, 0.5, 0.75, 1],
+      }
+    );
+
+    sectionRefs.current.forEach((section) => {
+      if (section) observer.observe(section);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [currentIndex, industriesLength]);
+
+  return { isMenuVisible, sectionRefs, isUserScrolling };
+};
