d56bffe9dc92a95c1ded46312715c3b998452d10
VasylPopaz <pafos1997@gmail.com>
2025-01-28 14:07:53 +0100

    Add mobile styles for the WhyChooseUs section

--- src/sections/Services/WhyChooseUs.tsx
+++ src/sections/Services/WhyChooseUs.tsx
@@ -9,15 +9,15 @@ export const WhyChooseUs = () => {
   const { theme } = useTheme();
 
   return (
-    <section className="bg-secondaryBgColor pb-[130px] pt-[100px]">
+    <section className="bg-secondaryBgColor py-[50px] md:pb-[130px] md:pt-[100px]">
       <motion.div className="container" {...slideInWithFade}>
         <h2 className="mb-[60px]">Why choose us</h2>
-        <ul className="flex justify-between">
+        <ul className="flex flex-col gap-y-[30px] md:flex-row md:justify-between">
           {whyChooseUsPoints.map((item, index) => {
             const imageTheme = theme === ThemeMode.DARK ? item.images.dark : item.images.light;
 
             return (
-              <li key={index} className="w-[250px]">
+              <li key={index} className="md:w-[250px]">
                 <img
                   srcSet={`${whyChooseUsImages[imageTheme[0] as keyof typeof whyChooseUsImages]} 1x, ${whyChooseUsImages[imageTheme[1] as keyof typeof whyChooseUsImages]} 2x`}
                   src={whyChooseUsImages[imageTheme[0] as keyof typeof whyChooseUsImages]}
@@ -25,7 +25,7 @@ export const WhyChooseUs = () => {
                   width={item.images.imageWidth}
                   height="170"
                   loading="lazy"
-                  className="mb-[40px] h-[170px] object-cover"
+                  className={`mx-auto mb-[40px] w-[${item.images.imageWidth}] h-[170px] object-cover md:mx-0 md:w-auto`}
                 />
                 <h4 className="mb-[10px]">{item.title}</h4>
                 <p className="small-text text-secondaryTextColor">{item.description}</p>
