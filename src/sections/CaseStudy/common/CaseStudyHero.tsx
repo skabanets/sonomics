0a13184b8a3bfc9891eca24672ddf02cd9bbaec7
VasylPopaz <pafos1997@gmail.com>
2025-01-29 11:14:38 +0100

    Add mobile styles for the CaseStudyHero section

--- src/sections/CaseStudy/common/CaseStudyHero.tsx
+++ src/sections/CaseStudy/common/CaseStudyHero.tsx
@@ -17,10 +17,10 @@ export const CaseStudyHero = ({ title, sectionImages }: CaseStudyHeroProps) => {
   } = images;
 
   return (
-    <section className="py-[80px]">
+    <section className="pb-[50px] pt-[30px] md:py-[80px]">
       <motion.div className="container" {...slideInWithFade}>
         <BackLink to={routes.CaseStudy} label="Back to case studies" className="mb-[20px]" />
-        <h1 className="mb-[60px] max-w-[1100px]">{title}</h1>
+        <h1 className="mb-[20px] md:mb-[60px] md:max-w-[1100px]">{title}</h1>
       </motion.div>
       <motion.ul className="full-hd-container flex gap-[40px]" {...slideInWithFade}>
         {sectionImages.map((item, index) => {
@@ -30,14 +30,14 @@ export const CaseStudyHero = ({ title, sectionImages }: CaseStudyHeroProps) => {
           const retinaImg = heroImages[retinaImage as keyof typeof heroImages];
 
           return (
-            <li key={index} className="w-1/3 even:mt-[4vw]">
+            <li key={index} className="hidden first:block md:block md:w-1/3 md:even:mt-[4vw]">
               <img
                 srcSet={`${img} 1x, ${retinaImg} 2x`}
                 src={image}
                 alt={`${title} image ${index + 1}`}
                 width={400}
                 height={210}
-                className="aspect-[400/210] w-full rounded-[20px] object-cover"
+                className="w-full rounded-[20px] object-cover md:aspect-[400/210]"
               />
             </li>
           );
