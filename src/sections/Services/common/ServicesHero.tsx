0ca40f3b83603b764251022be02c5d3bf3a30ddf
VasylPopaz <pafos1997@gmail.com>
2025-01-30 17:59:36 +0100

    Add mobile styles for the ServicesHero section

--- src/sections/Services/common/ServicesHero.tsx
+++ src/sections/Services/common/ServicesHero.tsx
@@ -40,19 +40,23 @@ export const ServicesHero = ({ title, hero }: ServicesHeroProps) => {
 
   return (
     <>
-      <section className="pb-[80px] pt-[100px]">
+      <section className="pb-[50px] pt-[30px] md:pb-[80px] md:pt-[100px]">
         <motion.div className="container" {...slideInWithFade}>
-          <BackLink to={routes.Services} label="Back to services" className="mb-[20px]" />
+          <BackLink
+            to={routes.Services}
+            label="Back to services"
+            className="mb-[10px] md:mb-[20px]"
+          />
           <h1
-            className={`mb-[30px] ${id === routes.AgileMethodologies.slice(10) ? "max-w-[1182px]" : "max-w-[918px]"}`}
+            className={`mb-[20px] md:mb-[30px] ${id === routes.AgileMethodologies.slice(10) ? "max-w-[1182px]" : "max-w-[918px]"}`}
           >
             {title}
           </h1>
-          <div className="flex items-center justify-between">
-            <div className="max-w-[675px]">
+          <div className="flex flex-col gap-y-[30px] md:flex-row md:items-center md:justify-between">
+            <div className="md:max-w-[675px]">
               {boldText && <h5 className="mb-[20px]">{boldText}</h5>}
               {text.map((text, index) => (
-                <p key={index} className="big-text mb-[40px] last:mb-[20px]">
+                <p key={index} className="big-text mb-[20px] first:mb-[30px] md:first:mb-[40px]">
                   {text}
                 </p>
               ))}
@@ -68,7 +72,7 @@ export const ServicesHero = ({ title, hero }: ServicesHeroProps) => {
               alt={title}
               width={imageWidth}
               height="406"
-              className={`rounded-t-[20px] object-cover ${id !== routes.CloudIntegration.slice(10) ? "rounded-b-[20px]" : ""}`}
+              className={`h-[300px] rounded-t-[20px] object-cover md:h-auto ${id !== routes.CloudIntegration.slice(10) ? "rounded-b-[20px]" : ""}`}
             />
           </div>
         </motion.div>
