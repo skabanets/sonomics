bf20bf3263f31fd2a7844dc5351a30ac64f93d45
Sergey Kabanets <skabanets97@gmail.com>
2025-02-01 01:01:52 +0200

    Add mobile styles for Services section on Home page

--- src/sections/Home/Services.tsx
+++ src/sections/Home/Services.tsx
@@ -4,18 +4,25 @@ import { motion } from "framer-motion";
 
 import { CustomTitle, Divider } from "../../components";
 
-import { services, routes, slideInWithFade } from "../../constants";
+import { routes, slideInWithFade } from "../../constants";
 
 export const Services = () => {
-  const servicesLinks = services.slice(0, 3);
+  const services = [
+    { name: "End-to-End Application Development", link: routes.EndToEndApplicationDevelopment },
+    { name: "Artificial Intelligence (AI) Integration", link: routes.ArtificialIntelligence },
+    { name: "Agile Methodologies", link: routes.AgileMethodologies },
+  ];
 
   return (
-    <section className="bg-secondaryBgColor py-[80px]">
-      <motion.div className="container flex flex-row justify-between" {...slideInWithFade}>
-        <h2 className="w-[412px] pt-[4px]">What we can do for our clients</h2>
-        <div className="flex w-[630px] flex-col gap-[25px]">
-          {servicesLinks.map((service) => (
-            <React.Fragment key={service.id}>
+    <section className="bg-secondaryBgColor py-[50px] md:py-[80px]">
+      <motion.div
+        className="container flex flex-col gap-[40px] md:flex-row md:justify-between md:gap-0"
+        {...slideInWithFade}
+      >
+        <h2 className="md:w-[412px] md:pt-[4px]">What we can do for our clients</h2>
+        <div className="flex w-full flex-col gap-[24px] md:w-[630px]">
+          {services.map((service, index) => (
+            <React.Fragment key={index}>
               <Divider />
               <Link to={service.link} className="group">
                 <CustomTitle
