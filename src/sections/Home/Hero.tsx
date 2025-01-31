d215dab111652279bf095c19d9205505de76530b
Sergey Kabanets <skabanets97@gmail.com>
2025-02-01 00:17:47 +0200

    Add mobile styles for Hero section on Home page

--- src/sections/Home/Hero.tsx
+++ src/sections/Home/Hero.tsx
@@ -7,9 +7,12 @@ import { CaseStudy } from "../../types";
 
 export const Hero = () => {
   return (
-    <section className="pt-[60px]">
-      <motion.div className="container flex gap-[80px]" {...slideInWithFade}>
-        <div className="flex w-[600px] flex-col gap-[22px] pt-[40px]">
+    <section className="pt-[30px] md:pt-[60px]">
+      <motion.div
+        className="container flex flex-col gap-[20px] md:flex-row md:gap-[80px]"
+        {...slideInWithFade}
+      >
+        <div className="flex w-full flex-col gap-[10px] md:w-[600px] md:gap-[22px] md:pt-[40px]">
           <h1>Empower your business with our leading IT solutions</h1>
           <h6 className="secondaryTextColor">
             Product Engineering, DevOps, Cloud, UI/UX, Quality Engineering, Mobile Development
