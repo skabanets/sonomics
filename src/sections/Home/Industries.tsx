e44154b170f42c302d3994b21783915e55f575a8
Sergey Kabanets <skabanets97@gmail.com>
2025-02-01 00:42:29 +0200

    Add mobile styles for IndustryCard component and Industries section on Home page

--- src/sections/Home/Industries.tsx
+++ src/sections/Home/Industries.tsx
@@ -14,11 +14,11 @@ import type { Industry } from "../../types";
 
 export const Industries = () => {
   return (
-    <section className="bg-blackBgColor py-[60px]">
-      <motion.div className="container space-y-12" {...slideInWithFade}>
+    <section className="bg-blackBgColor py-[50px] md:py-[60px]">
+      <motion.div className="container" {...slideInWithFade}>
         <h2 className="text-whiteTextColor">Industries</h2>
         <motion.ul
-          className="flex flex-row gap-5"
+          className="mt-[40px] flex flex-col gap-[20px] md:flex-row"
           variants={industriesContainerVariants}
           initial="hidden"
           whileInView="show"
@@ -36,15 +36,15 @@ export const Industries = () => {
           ))}
           <motion.li
             variants={cardVariants}
-            className="rounded-[20px] border border-solid border-transparent bg-darkBgColor transition hover:border-accentYellowColor focus-visible:border-accentYellowColor"
+            className="rounded-[15px] border border-solid border-transparent bg-darkBgColor transition hover:border-accentYellowColor focus-visible:border-accentYellowColor md:rounded-[20px]"
             tabIndex={0}
           >
             <Link
               to={routes.Industries}
-              className="flex h-[346px] w-[200px] items-center justify-center"
+              className="flex h-[80px] w-full items-center justify-center md:h-[346px] md:w-[200px]"
             >
               <CustomTitle
-                type="h4"
+                type="h5"
                 wrapperStyles="flex items-center gap-[10px]"
                 iconColor="fill-whiteTextColor"
                 titleStyles="text-whiteTextColor"
