06ac3353d67e5c02f4e7bfa77bf6bec4530e06ab
Sergey Kabanets <skabanets97@gmail.com>
2025-02-01 00:18:24 +0200

    Add mobile styles for WhyUs section om Home page

--- src/sections/Home/WhyUs.tsx
+++ src/sections/Home/WhyUs.tsx
@@ -1,30 +1,46 @@
+import { useMediaQuery } from "react-responsive";
 import { motion } from "framer-motion";
 
 import { slideInWithFade } from "../../constants";
 
-const whyUsAnimationProps = {
-  ...slideInWithFade,
-  transition: {
-    ...slideInWithFade.transition,
-    delay: 0.6,
-  },
-};
-
 export const WhyUs = () => {
+  const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
+
+  const whyUsAnimationProps = {
+    ...slideInWithFade,
+    transition: {
+      ...slideInWithFade.transition,
+      delay: 0.6,
+    },
+  };
+
+  const animation = isTablet ? whyUsAnimationProps : slideInWithFade;
+
   return (
-    <section className="py-[100px]">
-      <motion.div
-        className="container flex max-w-[1035px] flex-col items-center justify-center gap-2.5"
-        {...whyUsAnimationProps}
-      >
-        <h6 className="text-secondaryTextColor">Why Sonomics</h6>
-        <h2 className="text-center">
-          We&#8217;re not just about IT solutions — we&#8217;re about driving{" "}
-          <span className="text-accentYellowColor">
-            your business <br /> growth
-          </span>
-          , ensuring stability and <br /> fostering expertise
-        </h2>
+    <section className="py-[50px] md:py-[100px]">
+      <motion.div className="container" {...animation}>
+        <div className="flex w-full flex-col items-center justify-center gap-[10px] md:mx-auto md:w-[919px]">
+          <h6 className="text-secondaryTextColor">Why Sonomics</h6>
+          {isTablet ? (
+            <h2 className="text-center">
+              We&#8217;re not just about IT solutions — we&#8217;re about driving
+              <span className="text-accentYellowColor">
+                your business <br /> growth
+              </span>
+              , ensuring stability and <br /> fostering expertise
+            </h2>
+          ) : (
+            <h2 className="text-center">
+              We&#8217;re not just about IT <br />
+              solutions — we&#8217;re about driving
+              <span className="text-accentYellowColor">
+                <br />
+                your business growth
+              </span>
+              , ensuring stability and fostering expertise
+            </h2>
+          )}
+        </div>
       </motion.div>
     </section>
   );
