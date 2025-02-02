92400ea5a67db70362e6dc3f432a56c449d5eeb0
VasylPopaz <pafos1997@gmail.com>
2025-02-02 16:48:39 +0100

    Add CustomSwiper component

---
+++ src/components/CustomSwiper.tsx
@@ -0,0 +1,113 @@
+import { useRef, useState } from "react";
+import { motion, Variants } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import type { SwiperRef } from "swiper/react";
+
+import { Icon } from "../components";
+
+import type { WhyChooseSonomicsItem } from "../types";
+
+type SwiperItem = WhyChooseSonomicsItem | null;
+
+interface CustomSwiperProps {
+  items: SwiperItem[];
+  images: Record<string, string>;
+  animationProps: Variants;
+  isWhyChooseSonomics: boolean;
+}
+
+export const CustomSwiper = ({
+  items,
+  images,
+  animationProps,
+  isWhyChooseSonomics,
+}: CustomSwiperProps) => {
+  const [activeSlide, setActiveSlide] = useState<SwiperItem>(null);
+  const swiperRef = useRef<SwiperRef | null>(null);
+
+  const itemContent = (item: SwiperItem) => {
+    if (!item) return null;
+
+    if (isWhyChooseSonomics)
+      return (
+        <div className="flex rounded-[40px] bg-letsTalkBgColor transition">
+          <img
+            srcSet={`${images[item.image]} 1x, ${images[item.retinaImage]} 2x`}
+            src={images[item.image]}
+            alt={item.title}
+            width={120}
+            height={190}
+            loading="lazy"
+            className="h-auto rounded-l-[40px] object-cover"
+          />
+          <div className="flex flex-col justify-center gap-[7px] px-[15px] py-[28px]">
+            {" "}
+            <h5>{item.title}</h5>
+            <p className="small-text text-secondaryTextColor">{item.description}</p>
+          </div>
+        </div>
+      );
+  };
+
+  return (
+    <motion.div {...animationProps}>
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        onSlideChange={(swiper) => {
+          const currentSlide = items[swiper.realIndex];
+          setActiveSlide(currentSlide);
+        }}
+        className="mb-[20px]"
+        ref={swiperRef}
+      >
+        {items.map((item, index) => {
+          return <SwiperSlide key={index}>{itemContent(item)}</SwiperSlide>;
+        })}
+      </Swiper>
+      {activeSlide && (
+        <div className="flex h-[50px] items-center justify-between">
+          <button
+            onClick={() => swiperRef.current?.swiper.slidePrev()}
+            className="group flex size-[50px] items-center justify-center rounded-[10px] border border-mainTextColor transition hover:border-themeAccentColor focus-visible:border-themeAccentColor"
+            aria-label="Previous slide"
+          >
+            <Icon
+              id="arrow-left"
+              className="size-[15px] fill-mainTextColor transition group-hover:fill-themeAccentColor group-focus-visible:fill-themeAccentColor"
+            />
+          </button>
+          <div className="flex w-[40px] gap-[5px]">
+            {items.map((_, index) => (
+              <div
+                key={index}
+                className={`h-[3px] transition ${
+                  items.indexOf(activeSlide) === index
+                    ? "w-[15px] rounded-[70px] bg-mainTextColor"
+                    : "w-[3px] rounded-full bg-secondaryLightTextColor"
+                }`}
+              />
+            ))}
+          </div>
+          <button
+            onClick={() => swiperRef.current?.swiper.slideNext()}
+            className="group flex size-[50px] items-center justify-center rounded-[10px] border border-mainTextColor transition hover:border-themeAccentColor focus-visible:border-themeAccentColor"
+            aria-label="Next slide"
+          >
+            <Icon
+              id="arrow-right"
+              className="size-[15px] fill-mainTextColor transition group-hover:fill-themeAccentColor group-focus-visible:fill-themeAccentColor"
+            />
+          </button>
+        </div>
+      )}
+    </motion.div>
+  );
+};
--- src/sections/Services/common/ServicesWhyChooseSonomics.tsx
+++ src/sections/Services/common/ServicesWhyChooseSonomics.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
 
-import { Button, ContactForm, Modal } from "../../../components";
+import { Button, ContactForm, CustomSwiper, Modal } from "../../../components";
 
 import { useModal } from "../../../hooks";
 import { slideInWithFade } from "../../../constants";
@@ -13,6 +14,8 @@ interface ServicesWhyChooseSonomicsProps {
 
 export const ServicesWhyChooseSonomics = ({ content }: ServicesWhyChooseSonomicsProps) => {
   const [isOpen, toggleModal, handleClickOnBackdrop] = useModal();
+
+  const isMobile = useMediaQuery({ query: "(max-width: 767.98px)" });
   const {
     servicesDetails: { whyChooseSonomics },
   } = images;
@@ -20,17 +23,30 @@ export const ServicesWhyChooseSonomics = ({ content }: ServicesWhyChooseSonomics
 
   return (
     <>
-      <section className="py-[100px]">
-        <div className="container">
-          <motion.div className="mb-[40px] flex items-center justify-between" {...slideInWithFade}>
-            <h2 className="max-w-[852px] font-black leading-[58px]">{sectionTitle}</h2>
+      <section className="py-[50px] md:py-[100px]">
+        <div className="container relative">
+          <motion.div
+            className="mb-[50px] flex flex-col gap-y-[20px] md:mb-[40px] md:flex-row md:items-center md:justify-between"
+            {...slideInWithFade}
+          >
+            <h2 className="font-black md:max-w-[852px] md:leading-[58px]">{sectionTitle}</h2>
             <Button
               label="Let&#8217;s discuss your project"
               className="w-[300px]"
               onClick={toggleModal}
             />
           </motion.div>
-          <ul className="flex flex-wrap gap-[40px]">
+
+          {isMobile && (
+            <CustomSwiper
+              items={items}
+              images={whyChooseSonomics}
+              animationProps={slideInWithFade}
+              isWhyChooseSonomics
+            />
+          )}
+
+          <ul className="hidden md:flex md:flex-wrap md:gap-[40px]">
             {items.map((item, index) => {
               const img = whyChooseSonomics[item.image as keyof typeof whyChooseSonomics];
               const retinaImg =
@@ -38,7 +54,7 @@ export const ServicesWhyChooseSonomics = ({ content }: ServicesWhyChooseSonomics
               return (
                 <motion.li
                   key={index}
-                  className="flex w-[620px] rounded-[40px] bg-letsTalkBgColor transition"
+                  className="flex rounded-[40px] bg-letsTalkBgColor transition md:w-[620px]"
                   {...slideInWithFade}
                 >
                   <img
@@ -47,10 +63,10 @@ export const ServicesWhyChooseSonomics = ({ content }: ServicesWhyChooseSonomics
                     alt={sectionTitle}
                     width="286"
                     height="301"
-                    className={`rounded-l-[40px] bg-letsTalkBgColor object-cover`}
+                    className={`h-full w-[120px] rounded-l-[40px] bg-letsTalkBgColor object-cover md:h-auto md:w-auto`}
                     loading="lazy"
                   />
-                  <div className="flex flex-col justify-center gap-[10px] p-[30px]">
+                  <div className="flex flex-col justify-center gap-[7px] px-[15px] py-[28px] md:gap-[10px] md:p-[30px]">
                     {" "}
                     <h5>{item.title}</h5>
                     <p className="small-text text-secondaryTextColor">{item.description}</p>
