40d23ed61733b7433041ac0f782a1216a96702ce
VasylPopaz <pafos1997@gmail.com>
2025-02-02 16:48:17 +0100

    Update style for Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ label, className = "w-[170px]", ...rest }: ButtonProps)
   return (
     <button
       type="button"
-      className={`relative flex h-[50px] items-center justify-center gap-[10px] rounded-[90px] bg-accentRedColor px-[23px] py-[10px] text-[18px] font-medium leading-[1.5] text-whiteTextColor outline-none transition hover:bg-accentOrangeColor focus-visible:bg-accentOrangeColor ${className}`}
+      className={`relative flex h-[50px] items-center justify-center gap-[10px] rounded-[90px] bg-accentRedColor px-[23px] py-[10px] text-[18px] font-medium leading-[1.5] text-whiteTextColor outline-none transition hover:bg-accentOrangeColor focus-visible:bg-accentOrangeColor sm-max:w-full sm-max:text-[16px] ${className}`}
       {...rest}
     >
       {label}
