5f2dff962335c15d5e0c167bb6b0fd17ecf36e57
VasylPopaz <pafos1997@gmail.com>
2025-02-04 16:27:54 +0100

    Update Footer styles for tablet layout

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -6,20 +6,20 @@ import { socialLinks } from "../constants";
 
 export const Footer = () => {
   return (
-    <footer className="footer bg-blackBgColor pb-[40px] pt-[50px] md:pt-[80px]">
-      <div className="full-hd-container pb-[30px] md:flex md:pb-[40px]">
-        <div className="mb-[30px] flex flex-wrap space-y-[20px] md:mb-0 md:mr-auto md:w-[281px] md:space-y-[22px]">
+    <footer className="footer bg-blackBgColor pb-[40px] pt-[50px] lg:pt-[80px]">
+      <div className="full-hd-container tablet:gap-y-[30px] pb-[30px] md:flex md:flex-wrap md:justify-between md:pb-[40px]">
+        <div className="mb-[30px] flex flex-wrap space-y-[20px] md:mb-0 md:w-[420px] md:space-y-[22px] lg:mr-auto lg:w-[281px]">
           {" "}
           <Logo className="fill-whiteTextColor" />
           <h2 className="text-whiteTextColor">Let&#8217;s work together</h2>
         </div>
-        <nav className="mr-[120px] sm-max:mr-0 md:mr-[130px]">
+        <nav className="mr-[120px] sm-max:mr-0 md:mr-0 lg:mr-[130px]">
           <NavMenu
-            listClass=" flex w-full flex-wrap h-[106px] gap-x-[70px] gap-y-[40px] mb-[30px] md:mb-0 md:h-auto flex-col md:gap-[30px] text-whiteTextColor"
+            listClass="flex w-full flex-wrap h-[106px] gap-x-[70px] gap-y-[40px] mb-[30px] md:mb-0 md:justify-center lg:h-auto flex-col tablet:gap-[40px] lg:gap-[30px] text-whiteTextColor"
             linkClass="md:w-auto hover:text-accentYellowColor focus-visible:text-accentYellowColor"
           />
         </nav>
-        <div className="flex flex-col justify-between">
+        <div className="tablet:gap-y-[15px] tablet:flex-row tablet:items-end tablet:w-full flex flex-col justify-between">
           <address className="mb-[40px] not-italic sm-max:mb-[30px] md:mb-0">
             <p className="label mb-[15px] text-secondaryLightTextColor">Contact us</p>
             <ul className="bold-text flex flex-row gap-[50px] text-whiteTextColor sm-max:flex-wrap sm-max:gap-[15px] md:flex-col md:gap-[15px]">
@@ -57,7 +57,7 @@ export const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="full-hd-container relative left-0 flex items-center justify-between pt-[30px] before:absolute before:top-0 before:h-[1px] before:w-[calc(100vw-30px)] before:max-w-[360px] before:bg-grey600Color md:pt-[20px] md:before:w-[calc(100vw-160px)] lg:before:max-w-[1760px]">
+      <div className="full-hd-container relative left-0 flex items-center justify-between pt-[30px] before:absolute before:top-0 before:h-[1px] before:w-[calc(100vw-30px)] before:max-w-[360px] before:bg-grey600Color md:pt-[20px] md:before:max-w-[702px] lg:before:w-[calc(100vw-160px)] lg:before:max-w-[1760px]">
         <p className="small-text w-[186px] text-white sm-max:text-[14px] md:w-auto">
           &copy; 2025 Sonomics, Inc. All&nbsp;rights reserved
         </p>
