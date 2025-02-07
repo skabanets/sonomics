470409c9cfca43eb8d1fac4cf0feecdfdde35456
VasylPopaz <pafos1997@gmail.com>
2025-02-07 14:16:25 +0100

    Add tablet styles for the AboutSonomics section

--- src/sections/About/AboutSonomics.tsx
+++ src/sections/About/AboutSonomics.tsx
@@ -4,7 +4,7 @@ import { slideInWithFade } from "../../constants";
 
 export const AboutSonomics = () => {
   return (
-    <section className="pb-[20px] pt-[30px] md:pb-[80px] md:pt-[100px]">
+    <section className="pb-[20px] pt-[30px] md:pb-[50px] md:pt-[60px] lg:pb-[80px] lg:pt-[100px]">
       <motion.div className="container" {...slideInWithFade}>
         <h6 className="mb-[10px] text-secondaryTextColor md:mb-[20px]">About Sonomics</h6>
         <h1>
