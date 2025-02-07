91b3271eb85bd0adae4387b629f1c244d0b04897
VasylPopaz <pafos1997@gmail.com>
2025-02-07 14:16:48 +0100

    Add tablet styles for the OurMission section

--- src/sections/About/OurMission.tsx
+++ src/sections/About/OurMission.tsx
@@ -16,7 +16,7 @@ export const OurMission = () => {
 
   return (
     <section className="bg-ourMisionBgColor pb-[50px] md:pb-0">
-      <div className="mx-auto md:flex md:max-h-[600px]">
+      <div className="mx-auto md:flex lg:max-h-[600px]">
         <img
           srcSet={`${commonImages.our_mission_1x} 1x, ${commonImages.our_mission_2x} 2x`}
           src={commonImages.our_mission_1x}
@@ -26,7 +26,7 @@ export const OurMission = () => {
           className="mb-[50px] h-[260px] w-full object-cover md:mb-0 md:h-auto md:w-1/2"
         />
         <motion.div
-          className="mx-auto flex max-w-[390px] flex-col justify-center px-[15px] transition md:w-1/2 md:max-w-[768px] md:pl-[70px] md:pr-[150px] lg:max-w-none"
+          className="mx-auto flex max-w-[390px] flex-col justify-center px-[15px] transition md:w-1/2 md:max-w-[768px] md:p-[30px] lg:max-w-none lg:py-0 lg:pl-[70px] lg:pr-[150px]"
           {...ourMissionProps}
         >
           <h2 className="mb-[24px] text-whiteTextColor">Our mission</h2>
