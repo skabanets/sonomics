7ff590b352109510dd8b6262f30e3e4a0eaaad25
Sergey Kabanets <skabanets97@gmail.com>
2025-02-26 01:07:17 +0200

    Add ItemsMenu for IndustrySlides

--- src/sections/Industries/IndustrySlides.tsx
+++ src/sections/Industries/IndustrySlides.tsx
@@ -1,32 +1,13 @@
 import { motion } from "framer-motion";
 
-import { IndustrySliderCard } from "../../components";
+import { IndustrySliderCard, ItemsMenu } from "../../components";
 
-import { useDynamicDimensions, useMenuVisibility } from "../../hooks";
+import { useDynamicDimensions, useItemsMenuNavigation } from "../../hooks";
 import { industries, slideInWithFade } from "../../constants";
+import type { CaseStudyMenuItem } from "../../types";
 
 export const IndustrySlides = () => {
-  const { isMenuVisible, sectionRefs, isUserScrolling, currentIndex, setCurrentIndex } =
-    useMenuVisibility(industries.length);
   const { slidePadding, slideHeight } = useDynamicDimensions();
-
-  const handleMenuClick = (index: number) => {
-    isUserScrolling.current = true;
-
-    setCurrentIndex(index);
-
-    const targetSection = sectionRefs.current[index];
-    if (targetSection) {
-      targetSection.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-
-      setTimeout(() => {
-        isUserScrolling.current = false;
-      }, 1000);
-    }
-  };
 
   const getIndustrySliderBgColor = (index: number) => {
     switch (index) {
@@ -42,8 +23,23 @@ export const IndustrySlides = () => {
     }
   };
 
+  const industryOptions: CaseStudyMenuItem[] = industries.map((option) => ({
+    id: option.name,
+    name: option.name,
+  }));
+
+  const { currentIndex, handleMenuClick } = useItemsMenuNavigation({
+    menuOptions: industryOptions,
+  });
+
   return (
     <div className="relative">
+      <ItemsMenu
+        items={industryOptions}
+        currentIndex={currentIndex}
+        handleMenuClick={handleMenuClick}
+        styles="sticky top-0 opacity-100 z-50"
+      />
       {industries.map((industry, index) => {
         const slideBackgroundColor = getIndustrySliderBgColor(index);
 
@@ -51,9 +47,7 @@ export const IndustrySlides = () => {
           <section
             key={index}
             className={`h-dvh py-[${slidePadding}px] transition ${slideBackgroundColor}`}
-            ref={(el) => {
-              sectionRefs.current[index] = el as HTMLDivElement | null;
-            }}
+            id={industry.name}
           >
             <motion.div
               {...slideInWithFade}
@@ -65,39 +59,6 @@ export const IndustrySlides = () => {
           </section>
         );
       })}
-
-      {isMenuVisible && (
-        <ul className="fixed right-[38px] top-1/2 flex translate-y-[-50%] flex-col gap-[15px]">
-          {industries.map((industry, index) => (
-            <li
-              key={index}
-              className="group relative cursor-pointer transition"
-              onClick={() => handleMenuClick(index)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter" || e.key === " ") {
-                  e.preventDefault();
-                  handleMenuClick(index);
-                }
-              }}
-              tabIndex={0}
-              aria-label={`Menu item: ${industry.name}`}
-            >
-              <span
-                className={`absolute right-[15px] top-1/2 translate-y-[-50%] whitespace-nowrap rounded-[10px] bg-navMenuBgColor px-[7px] py-[5px] text-mainTextColor transition group-hover:opacity-100 group-focus-visible:opacity-100 ${currentIndex === index ? "opacity-100" : "opacity-0"}`}
-              >
-                {industry.name}
-              </span>
-              <div
-                className={`transition ${
-                  currentIndex === index
-                    ? `${currentIndex === 3 ? "bg-whiteTextColor" : "bg-mainTextColor"} h-[35px] w-[9px] rounded-[70px]`
-                    : "h-[10px] w-[10px] rounded-full bg-secondaryLightTextColor group-hover:border group-hover:border-mainTextColor group-focus-visible:border group-focus-visible:border-mainTextColor"
-                }`}
-              />
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 };
