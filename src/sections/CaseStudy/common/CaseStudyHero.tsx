743f704535efc3f2f9b40525a5bff9ae123701bf
Sergey Kabanets <skabanets97@gmail.com>
2025-02-26 00:43:38 +0200

    Add highlightText helper

--- src/sections/CaseStudy/common/CaseStudyHero.tsx
+++ src/sections/CaseStudy/common/CaseStudyHero.tsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { BackLink } from "../../../components";
 
 import { routes, slideInWithFade } from "../../../constants";
+import { highlightText } from "../../../helpers";
 import type { pageTitle } from "../../../types";
 
 interface CaseStudyHeroProps {
@@ -10,27 +11,11 @@ interface CaseStudyHeroProps {
 }
 
 export const CaseStudyHero = ({ title }: CaseStudyHeroProps) => {
-  const renderHighlightedText = (text: string, accentWords: string[]) => {
-    const regex = new RegExp(`(${accentWords.join("|")})`, "gi");
-
-    return text.split(regex).map((part, index) =>
-      accentWords.includes(part) ? (
-        <span key={index} className="text-themeAccentColor">
-          {part}
-        </span>
-      ) : (
-        part
-      )
-    );
-  };
-
   return (
     <section className="pb-[50px] pt-[30px] md:py-[50px] lg:py-[55px]">
       <motion.div className="container" {...slideInWithFade}>
         <BackLink to={routes.CaseStudy} label="Back to case studies" className="mb-[20px]" />
-        <h1 className="lg:max-w-[1090px]">
-          {renderHighlightedText(title.text, title.accentWords)}
-        </h1>
+        <h1 className="lg:max-w-[1090px]">{highlightText(title.text, title.accentWords)}</h1>
       </motion.div>
     </section>
   );
--- src/sections/Services/common/ServicesExpertise.tsx
+++ src/sections/Services/common/ServicesExpertise.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { motion } from "framer-motion";
 
 import { routes, slideInWithFade } from "../../../constants";
+import { highlightText } from "../../../helpers";
 import type { Expertise } from "../../../types";
 
 interface ServiceExpertiseProps {
@@ -11,27 +12,13 @@ interface ServiceExpertiseProps {
 export const ServicesExpertise = ({ expertise }: ServiceExpertiseProps) => {
   const { id } = useParams();
 
-  const renderHighlightedText = (text: string, accentWords: string[]) => {
-    const regex = new RegExp(`(${accentWords.join("|")})`, "gi");
-
-    return text.split(regex).map((part, index) =>
-      accentWords.includes(part) ? (
-        <span key={index} className="text-accentYellowColor">
-          {part}
-        </span>
-      ) : (
-        part
-      )
-    );
-  };
-
   return (
     <section
       className={`${id === routes.CloudIntegration.slice(10) ? "md:pt-[40px]" : "md:pt-[50px] lg:pt-[100px]"} py-[50px] md:pb-[50px] lg:pb-[100px]`}
     >
       <motion.div className="container" {...slideInWithFade}>
         <h2 className="text-center md:mx-auto md:max-w-[700px] lg:max-w-[920px]">
-          {renderHighlightedText(expertise.text, expertise.accentWords)}
+          {highlightText(expertise.text, expertise.accentWords)}
         </h2>
       </motion.div>
     </section>
