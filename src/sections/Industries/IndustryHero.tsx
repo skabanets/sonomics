ac3c33b9019b591f3490da293af05193e737f70d
Sergey Kabanets <skabanets97@gmail.com>
2025-02-27 20:17:43 +0200

    Add transition effect for highlighted text in hero sections

--- src/sections/Industries/IndustryHero.tsx
+++ src/sections/Industries/IndustryHero.tsx
@@ -7,8 +7,8 @@ export const IndustryHero = () => {
     <section className="pb-[40px] pt-[60px]">
       <motion.div className="container" {...slideInWithFade}>
         <h1 className="max-w-[1200px]">
-          We deliver <span className="text-themeAccentColor">real value</span> to clients because of
-          our knowledge of the industries in which they operate
+          We deliver <span className="text-themeAccentColor transition">real value</span> to clients
+          because of our knowledge of the industries in which they operate
         </h1>
       </motion.div>
     </section>
