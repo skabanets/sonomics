46b3b0c901176c8bd970a9fce79f37da3bd5cc40
Sergey Kabanets <skabanets97@gmail.com>
2025-02-28 00:14:29 +0200

    Update accent colors for Home page

--- src/sections/Home/CaseStudies.tsx
+++ src/sections/Home/CaseStudies.tsx
@@ -27,8 +27,8 @@ export const CaseStudies = () => {
             <CustomTitle
               type="h5"
               wrapperStyles="flex items-center gap-[10px]"
-              iconColor="fill-accentRedColor"
-              titleStyles="text-accentRedColor"
+              iconColor="fill-themeAccentColor"
+              titleStyles="text-themeAccentColor"
             />
           </Link>
         </motion.div>
