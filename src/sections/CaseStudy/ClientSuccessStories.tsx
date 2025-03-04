1c3a7dab51c87853c2b0a05227c8bdf700ab2af6
Sergey Kabanets <skabanets97@gmail.com>
2025-03-04 22:07:38 +0200

    Add adaptive for ClientSuccessStories section

--- src/sections/CaseStudy/ClientSuccessStories.tsx
+++ src/sections/CaseStudy/ClientSuccessStories.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
 
 import { CaseCard } from "../../components";
 
@@ -10,14 +11,29 @@ export interface ClientSuccessStorieProps {
 }
 
 export const ClientSuccessStories = ({ cases }: ClientSuccessStorieProps) => {
+  const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width: 1440px)" });
+
+  const imagePropertiesMap = {
+    desktop: { containerStyles: "w-[600px]", imageWidth: 600, imageHeight: 500 },
+    tablet: { containerStyles: "w-[332px]", imageWidth: 332, imageHeight: 300 },
+    mobile: { containerStyles: "w-full", imageWidth: 360, imageHeight: 300 },
+  };
+
+  const imageProperties = isDesktop
+    ? imagePropertiesMap.desktop
+    : isTablet
+      ? imagePropertiesMap.tablet
+      : imagePropertiesMap.mobile;
+
   return (
-    <section className="pb-[100px] pt-[80px]">
+    <section className="py-[30px] lg:pb-[100px] lg:pt-[80px]">
       <motion.div className="container" {...slideInWithFade}>
         <h1>Client success stories</h1>
-        <ul className="mt-[80px] flex flex-wrap justify-between gap-y-[60px]">
+        <ul className="mt-[30px] flex flex-col flex-wrap gap-[40px] md:flex-row md:justify-between lg:mt-[80px] lg:gap-y-[60px]">
           {cases.map((caseStudy) => (
             <motion.li key={caseStudy.id} {...slideInWithFade}>
-              <CaseCard caseStudy={caseStudy as CaseStudy} />
+              <CaseCard caseStudy={caseStudy as CaseStudy} {...imageProperties} />
             </motion.li>
           ))}
         </ul>
