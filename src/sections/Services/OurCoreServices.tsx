ca17b1f139400c245e75871f25e7f1798b9b9c95
Sergey Kabanets <skabanets97@gmail.com>
2025-03-05 00:11:14 +0200

    Add adaptive for ServiceCard and OurCoreServices section

--- src/sections/Services/OurCoreServices.tsx
+++ src/sections/Services/OurCoreServices.tsx
@@ -2,15 +2,21 @@ import { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
 
 import { Icon, ServiceCard } from "../../components";
 
 import { services, slideInWithFade } from "../../constants";
+import type { Service } from "../../types";
 
 export const OurCoreServices = () => {
+  const [activeSlide, setActiveSlide] = useState<Service | null>(services[0]);
   const swiperRef = useRef<any>(null);
   const [isBeginning, setIsBeginning] = useState(true);
   const [isEnd, setIsEnd] = useState(false);
+
+  const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width: 1440px)" });
 
   const handleSwiper = (swiper: any) => {
     swiperRef.current = swiper;
@@ -19,53 +25,56 @@ export const OurCoreServices = () => {
   };
 
   return (
-    <section className="bg-secondaryBgColor pt-[100px]">
+    <section className="bg-secondaryBgColor pt-[50px] lg:pt-[100px]">
       <motion.div className="container flex flex-col" {...slideInWithFade}>
         <div className="flex items-center justify-between">
           <h2>Our core services</h2>
-          <div className="flex gap-[20px]">
-            <button
-              onClick={() => swiperRef.current?.slidePrev()}
-              className={`group flex h-[50px] w-[50px] items-center justify-center rounded-[10px] border border-solid transition ${isBeginning ? "cursor-not-allowed border-disabledBtnColor bg-formFieldBgColor" : "border-darkBorderColor bg-navMenuBgColor hover:border-themeAccentColor focus-visible:border-themeAccentColor"}`}
-              aria-label="Previous slide"
-              disabled={isBeginning}
-            >
-              <Icon
-                id="arrow-left-bold"
-                className={`h-[15px] w-[15px] transition ${isBeginning ? "fill-disabledBtnColor" : "fill-inverseColor group-hover:fill-themeAccentColor group-focus-visible:fill-themeAccentColor"}`}
-              />
-            </button>
-            <button
-              onClick={() => swiperRef.current?.slideNext()}
-              className={`group flex h-[50px] w-[50px] items-center justify-center rounded-[10px] border border-solid transition ${isEnd ? "cursor-not-allowed border-disabledBtnColor bg-formFieldBgColor" : "border-darkBorderColor bg-navMenuBgColor hover:border-themeAccentColor focus-visible:border-themeAccentColor"}`}
-              aria-label="Next slide"
-              disabled={isEnd}
-            >
-              <Icon
-                id="arrow-right-bold"
-                className={`h-[15px] w-[15px] transition ${isEnd ? "fill-disabledBtnColor" : "fill-inverseColor group-hover:fill-themeAccentColor group-focus-visible:fill-themeAccentColor"}`}
-              />
-            </button>
-          </div>
+          {isTablet && (
+            <div className="flex gap-[20px]">
+              <button
+                onClick={() => swiperRef.current?.slidePrev()}
+                className={`group flex h-[50px] w-[50px] items-center justify-center rounded-[10px] border border-solid transition ${isBeginning ? "cursor-not-allowed border-disabledBtnColor bg-formFieldBgColor" : "border-darkBorderColor bg-navMenuBgColor hover:border-themeAccentColor focus-visible:border-themeAccentColor"}`}
+                aria-label="Previous slide"
+                disabled={isBeginning}
+              >
+                <Icon
+                  id="arrow-left-bold"
+                  className={`h-[15px] w-[15px] transition ${isBeginning ? "fill-disabledBtnColor" : "fill-inverseColor group-hover:fill-themeAccentColor group-focus-visible:fill-themeAccentColor"}`}
+                />
+              </button>
+              <button
+                onClick={() => swiperRef.current?.slideNext()}
+                className={`group flex h-[50px] w-[50px] items-center justify-center rounded-[10px] border border-solid transition ${isEnd ? "cursor-not-allowed border-disabledBtnColor bg-formFieldBgColor" : "border-darkBorderColor bg-navMenuBgColor hover:border-themeAccentColor focus-visible:border-themeAccentColor"}`}
+                aria-label="Next slide"
+                disabled={isEnd}
+              >
+                <Icon
+                  id="arrow-right-bold"
+                  className={`h-[15px] w-[15px] transition ${isEnd ? "fill-disabledBtnColor" : "fill-inverseColor group-hover:fill-themeAccentColor group-focus-visible:fill-themeAccentColor"}`}
+                />
+              </button>
+            </div>
+          )}
         </div>
-        <div className="border-b border-solid border-mainTextColor pb-[89px]">
-          <div className="mt-[60px] flex overflow-hidden">
+        <div className="relative mt-[30px] border-b border-solid border-mainTextColor pb-[120px] md:pb-[49px] lg:mt-[60px] lg:pb-[89px]">
+          <div className="flex overflow-hidden">
             <Swiper
               modules={[Navigation]}
-              spaceBetween={40}
+              spaceBetween={isDesktop ? 40 : 20}
               slidesPerView="auto"
-              slidesPerGroup={2}
+              slidesPerGroup={isDesktop ? 2 : 1}
               loop={false}
               onSwiper={(swiper) => handleSwiper(swiper)}
               onSlideChange={(swiper) => {
                 setIsBeginning(swiper.isBeginning);
                 setIsEnd(swiper.isEnd);
+                setActiveSlide(services[swiper.realIndex]);
               }}
             >
               {services.map((service) => (
                 <SwiperSlide
                   key={service.id}
-                  className="h-[436px] w-[400px] rounded-[10px] border border-solid border-darkBorderColor bg-navMenuBgColor p-[30px] transition hover:bg-secondaryBgColor focus-visible:bg-secondaryBgColor"
+                  className="h-[436px] w-full rounded-[10px] border border-solid border-darkBorderColor bg-navMenuBgColor p-[30px] transition hover:bg-secondaryBgColor focus-visible:bg-secondaryBgColor md:h-[444px] md:w-[342px] lg:h-[436px] lg:w-[400px]"
                   tabIndex={0}
                 >
                   <ServiceCard service={service} />
@@ -73,6 +82,44 @@ export const OurCoreServices = () => {
               ))}
             </Swiper>
           </div>
+          {activeSlide && !isTablet && (
+            <div className="absolute bottom-[50px] z-50 flex w-full items-center justify-between">
+              <button
+                onClick={() => swiperRef.current?.slidePrev()}
+                className={`group flex h-[50px] w-[50px] items-center justify-center rounded-[10px] border border-solid transition ${isBeginning ? "cursor-not-allowed border-disabledBtnColor bg-formFieldBgColor" : "border-darkBorderColor bg-navMenuBgColor hover:border-themeAccentColor focus-visible:border-themeAccentColor"}`}
+                aria-label="Previous slide"
+                disabled={isBeginning}
+              >
+                <Icon
+                  id="arrow-left-bold"
+                  className={`h-[15px] w-[15px] transition ${isBeginning ? "fill-disabledBtnColor" : "fill-inverseColor group-hover:fill-themeAccentColor group-focus-visible:fill-themeAccentColor"}`}
+                />
+              </button>
+              <div className="flex gap-[5px]">
+                {services.map((slide, index) => (
+                  <div
+                    key={slide.id}
+                    className={`h-[3px] transition ${
+                      services.indexOf(activeSlide) === index
+                        ? "w-[15px] rounded-[70px] bg-mainTextColor"
+                        : "w-[3px] rounded-full bg-secondaryLightTextColor"
+                    }`}
+                  />
+                ))}
+              </div>
+              <button
+                onClick={() => swiperRef.current?.slideNext()}
+                className={`group flex h-[50px] w-[50px] items-center justify-center rounded-[10px] border border-solid transition ${isEnd ? "cursor-not-allowed border-disabledBtnColor bg-formFieldBgColor" : "border-darkBorderColor bg-navMenuBgColor hover:border-themeAccentColor focus-visible:border-themeAccentColor"}`}
+                aria-label="Next slide"
+                disabled={isEnd}
+              >
+                <Icon
+                  id="arrow-right-bold"
+                  className={`h-[15px] w-[15px] transition ${isEnd ? "fill-disabledBtnColor" : "fill-inverseColor group-hover:fill-themeAccentColor group-focus-visible:fill-themeAccentColor"}`}
+                />
+              </button>
+            </div>
+          )}
         </div>
       </motion.div>
     </section>
